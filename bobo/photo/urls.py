5aeaf68336a70cf482546650f2b7f0ef2f34fb5f
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-26 17:03:41 +0200

    Update urls.py

--- bobo/photo/urls.py
+++ bobo/photo/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path(r'/', views.home),
     path(r'/navebarre_coupe', views.navebarre_coupe),
     path(r'/navebarre_habits', views.navebarre_habits),
+    path(r'/database_mode/', views.database_mode),
+    path(r'/tendance/', views.tendance),
+    path(r'/navebarre_admin2/', views.navebarre_admin2),
+    
     ]
