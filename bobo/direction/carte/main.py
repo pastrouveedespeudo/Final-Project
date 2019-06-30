99071ead0bb21932e894e4078c25a60685799e40
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-30 15:40:12 +0200

    Update main.py

--- bobo/direction/carte/main.py
+++ bobo/direction/carte/main.py
@@ -1,3 +1,7 @@
+"""I cant use reverse call localisation
+So i must intermediate views with direction functions
+with a main... (runner() is the problem)"""
+
 
 from direction.ville import ville
 from direction.nouvel_pos import long_lat
@@ -11,86 +15,40 @@ from direction.addresse import *
 if __name__=="__main__": 
   
 
-
     def runner(liste):
-
 
         new_lat = ''
         new_long = ''
-
 
         while True:
 
             for i in liste:
                 
                 if new_lat != '' and new_long != '':
-                    
-                    print('de lattitude:', new_lat, 'et de longitude', new_long)
-                    
-                    adresse = dress_to_ville(new_lat, new_long)
-          
-                    print('adresse trouvée :', adresse[0])
 
+                    adresse = dress_to_ville(new_lat, new_long)
                     position_vent = vent_deux(adresse[0])
-                    
-                    vent = superficie_ville(adresse[1])
-                    print(vent,'48498749848')
-                    
-                    
-                    
-                else:
-             
-                    lat, long = ville(i)
-               
-                    adresse = dress_to_ville(lat, long)
-                  
-                    print('de lattitude:', lat, 'de longitude', long)
-                    
-                    position_vent = vent_deux(i)
-                    
                     vent = superficie_ville(adresse[1])
                     print(vent)
 
-                    
-                   
-                print(vent)
-                print('donc le vent va vers:', position_vent)
-               
-                
-               
-                
+                else:
+             
+                    lat, long = ville(i)
+                    adresse = dress_to_ville(lat, long)
+                    position_vent = vent_deux(i)
+                    vent = superficie_ville(adresse[1])
+                    print(vent)
+
+
                 if new_lat != '' and new_long != '':
                     
                     nouvelle_position = long_lat(new_lat, new_long, vent, position_vent)
-                    print('la nouvelle position est de :', nouvelle_position)
                     
                 else:
                           
-                    nouvelle_position = long_lat(lat, long, vent, position_vent)
-                    print('la nouvelle position est de :', nouvelle_position)    
+                    nouvelle_position = long_lat(lat, long, vent, position_vent)  
                 
                 new_lat = nouvelle_position[0]
                 new_long = nouvelle_position[1]
 
                 
-                print('\n')
-
-    runner(["strasbourg"])
-    #runner("marseille")
-                
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
