b6e9b7cfaae6fefe11baaf580972fef5ed9d2d93
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-30 15:41:57 +0200

    Add files via upload

---
+++ bobo/direction/views_function.py
@@ -0,0 +1,101 @@
+"""Sub function of views"""
+
+from .carte.direction.ville import ville
+from .carte.direction.nouvel_pos import long_lat
+from .carte.direction.boussole import calcul_vent
+from .carte.direction.vent import *
+from .carte.direction.superficie import superficie_ville
+from .carte.direction.addresse import dress_to_ville
+
+
+def function_map(data):
+    """Calling all function"""
+    
+    lat, long = ville(data)
+
+    adresse = dress_to_ville(lat, long)
+
+    position_vent = vent_deux(data)
+
+    vent = superficie_ville(adresse[1])
+
+    nouvelle_position = long_lat(lat, long, vent, position_vent)
+
+    return nouvelle_position, lat, long
+
+
+def function_map2(data2):
+    """Transform , to ."""
+    
+    lat = ''
+    long = ''
+    listee = []
+    
+    for i in data2:
+        if i == ',':
+            break
+        elif i == '.':
+            lat += i
+        else:
+            lat += i
+            
+    listee = []
+    c = 0
+    for i in data2:
+        
+        if i == ',':
+            index = c
+        else:
+            listee.append(i)
+        c+=1
+
+    return lat, long, listee, index
+
+
+def function_map3(lat, long, listee, index, data):
+    """Second function if the trials insn't
+    the first one"""
+
+    lat = float(lat)
+    
+    long = listee[index:-1]
+    
+    long = "".join(long)
+    long = float(long)
+
+
+    adresse = dress_to_ville(lat, long)
+
+    
+    position_vent = vent_deux(data)
+
+    vent = superficie_ville(adresse[1])
+
+    nouvelle_position = long_lat(lat, long, vent, position_vent)
+
+
+    return nouvelle_position, lat, long
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
