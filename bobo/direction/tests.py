554ac52fbedf8975a5b2afb0139d5614506d27d3
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-01 18:04:16 +0200

    Update tests.py
    pylint mtn, mais javais pas vu je dois le convaincre, réfléchis, ce soir affine ton text de readme ca sera ta base, en gros enjeu sociétal pour moi la société c le groupe, et le groupe c lentraide. on veut aider les femmes bon tu va dire ca ca fait bizzare mais tu peux pas dire j'ai eu la flemme de faire les mecs... dont faut chercher un texte qui dit que les femmes sont moins sur delles pour le visage et ca chai pas si ca va passer au pire avoir son diplome et passer pour un miso? ok et ensuite tu dis ouais la mode second facteur pollu, ah oui tu dis aussi, la société nous apprend a faire des these anti these cherche un truk comme ca

--- bobo/direction/tests.py
+++ bobo/direction/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class Test(TestCase):
+    
+    def test_map_page(self):
+        response = self.client.get(reverse('map'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_navebarre_page(self):
+        response = self.client.get(reverse('navebarre_vent'))
+        self.assertEqual(response.status_code, 200)
+
