0942befca76cfa4136ab70c2bf129bab6b8ebbb6
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-01 20:41:09 +0200

    Update urls.py

--- bobo/photo/urls.py
+++ bobo/photo/urls.py
@@ -3,14 +3,13 @@ from . import views
 from django.conf.urls import include
 
 urlpatterns = [
-    path(r'/coupe', views.coupe),
-    path(r'/habits', views.habits),
-    path(r'/home_bobo', views.home_bobo),
-    path(r'/', views.home),
-    path(r'/navebarre_coupe', views.navebarre_coupe),
-    path(r'/navebarre_habits', views.navebarre_habits),
-    path(r'/database_mode/', views.database_mode),
-    path(r'/tendance/', views.tendance),
-    path(r'/navebarre_admin2/', views.navebarre_admin2),
+    path(r'/', views.home, name = 'home'),
+    path(r'/coupe', views.coupe, name = 'coupe'),
+    path(r'/navebarre_coupe', views.navebarre_coupe, name = 'navebarre_coupe'),
+    path(r'/habits', views.habits, name = 'habits'),
+    path(r'/navebarre_habits', views.navebarre_habits, name = 'navebarre_habits'),
+    path(r'/tendance', views.tendance, name = 'tendance'),
+    path(r'/navebarre_admin2', views.navebarre_admin2, name = 'navebarre_admin2'),
+    path(r'/database_mode/', views.database_mode, name = 'database_mode'),
     
     ]
