831484b7dc34ec2f42541c42a97faf895b84f18c
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-04 01:48:23 +0200

    Update graphe.js

--- bobo/static/js/pollution/graphe.js
+++ bobo/static/js/pollution/graphe.js
@@ -64,4 +64,78 @@ function two(div, ville){
     document.getElementById(div).style.background = "black";
     document.getElementById(ville).innerHTML = '';
 };
+function graphe(legraphe, condition){
+  
+  var a = "<i><a href='/polution/info_pollu' id='sav'>" + "En savoir plus sur la condition: " + condition + "</a></i>";
 
+  
+  document.getElementById('savoir').innerHTML = a;
+
+  LISTE_AJAX_TYPE_GRAPHE.push(legraphe)
+     document.getElementById('graphique1').innerHTML = '';
+     jQuery.ajax({
+       data:{
+           'city':LISTE_AJAX[LISTE_AJAX.length - 1],
+           'graph':LISTE_AJAX_TYPE_GRAPHE[LISTE_AJAX_TYPE_GRAPHE.length - 1]},
+
+       
+              type:"POST",
+              url:"/polution/graphe"
+          })
+          .done(function(data){
+              if (data.error){
+                  jQuery("#monCadreAlert").text(data.error);
+                  jQuery("#is_save");
+                  
+                  
+              }
+              else{
+                  jQuery("#is_save").html(data.data);
+                  jQuery("#monCadreAlert");
+             
+                  document.getElementById('graphique1').innerHTML = "<img id='dia' src = '/static/popo/" +
+                                                                    data +
+                                                                    "'>"
+
+       };
+              
+   });
+};
+
+
+$(document).ready(function(){
+
+    
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+
+
+
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+});
