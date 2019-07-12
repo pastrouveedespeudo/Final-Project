27f2081df81ff3a5cdb51706b6b91aabc7b193b0
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-12 21:15:00 +0200

    Update CONFIG.py
    merde jai oublié ca bon plusqua mettre les data dans la database et les affiché et voila et dire pk on peut pas reactuliser les info
    
    sinon trouver comment mette un link et le mec change juste les textes.... sinon je dis c un blog personnel et c mon blog et comme c le mien
    je sais comment faire

--- bobo/direction/CONFIG.py
+++ bobo/direction/CONFIG.py
@@ -1,7 +1,13 @@
-liste = ["connard", "encule", "gros enculee", "pd",
-         "con", "nique", "fils de pute", "salop",
-         "salope", "merde", "chié",
-         "trou du cul", "cul", "bite",
-         "batard", "batar", "merde",
-         ""
-         ]
+GROS_MOTS = ["connard", "encule", "gros enculee", "pd",
+             "con", "nique", "fils de pute", "salop",
+             "salope", "merde", "chié",
+             "trou du cul", "cul", "bite",
+             "batard", "batar", "merde",
+             ]
+
+
+
+HOST = "ec2-46-137-91-216.eu-west-1.compute.amazonaws.com"
+USER = "vobmeocqilozfo"
+PASSWORD = "81886977eb54552e57161a1afcb236fb4a21ad3681abbad6921d72f6c41322c6"
+DATABASE = "d9d5rats8b1tc1"
