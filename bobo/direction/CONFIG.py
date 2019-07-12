4e6d3c7e15574aa77fad3f9f16ce828246533d30
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-12 21:05:27 +0200

    Add files via upload
    ben jcroyais en connaitre plein mais....

---
+++ bobo/direction/CONFIG.py
@@ -0,0 +1,7 @@
+liste = ["connard", "encule", "gros enculee", "pd",
+         "con", "nique", "fils de pute", "salop",
+         "salope", "merde", "chié",
+         "trou du cul", "cul", "bite",
+         "batard", "batar", "merde",
+         ""
+         ]
