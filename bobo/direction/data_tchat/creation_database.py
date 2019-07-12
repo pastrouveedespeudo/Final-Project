f1adcde25faf005d9f9bc4079eea6c14ef4209f5
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-12 22:25:47 +0200

    Update creation_database.py

--- bobo/direction/data_tchat/creation_database.py
+++ bobo/direction/data_tchat/creation_database.py
@@ -30,28 +30,4 @@ def create_database():
 
 
 
-def database1():
-    
 
-    conn = psycopg2.connect(database=DATABASE,
-                            user=USER,
-                            host=HOST,
-                            password=PASSWORD) 
-
-    cur = conn.cursor()
-    
-    cur.execute("""
-                select * from tchat_map;
-                """)
-
-    
-    conn.commit()
-    
-    rows = cur.fetchall()
-    liste = [i for i in rows]
-
-    for i in liste:
-        print(i)
-        print('\n')
-
-database1()
