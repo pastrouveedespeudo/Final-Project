dfb302d37538d3ac45c481e1b8a1452ecf8fd9a8
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-14 00:00:05 +0200

    Update main.py

--- bobo/direction/carte/main.py
+++ bobo/direction/carte/main.py
@@ -7,13 +7,12 @@ from direction.ville import ville
 from direction.nouvel_pos import long_lat
 from direction.boussole import calcul_vent
 from direction.vent import *
-
 from direction.superficie import *
 from direction.addresse import *
 
 
-if __name__=="__main__": 
-  
+if __name__ == "__main__":
+    """we lunch the script"""
 
     def runner(liste):
 
@@ -23,7 +22,7 @@ if __name__=="__main__":
         while True:
 
             for i in liste:
-                
+
                 if new_lat != '' and new_long != '':
 
                     adresse = dress_to_ville(new_lat, new_long)
@@ -32,7 +31,7 @@ if __name__=="__main__":
                     print(vent)
 
                 else:
-             
+
                     lat, long = ville(i)
                     adresse = dress_to_ville(lat, long)
                     position_vent = vent_deux(i)
@@ -41,14 +40,17 @@ if __name__=="__main__":
 
 
                 if new_lat != '' and new_long != '':
-                    
-                    nouvelle_position = long_lat(new_lat, new_long, vent, position_vent)
-                    
+
+                    nouvelle_position = long_lat(new_lat,
+                                                 new_long,
+                                                 vent,
+                                                 position_vent)
+
                 else:
-                          
-                    nouvelle_position = long_lat(lat, long, vent, position_vent)  
-                
+                    nouvelle_position = long_lat(lat,
+                                                 long,
+                                                 vent,
+                                                 position_vent)
+
                 new_lat = nouvelle_position[0]
                 new_long = nouvelle_position[1]
-
-                
