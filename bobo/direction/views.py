24a76020b884baf0804b372710c70b465739aa03
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-14 00:07:15 +0200

    Update views.py

--- bobo/direction/views.py
+++ bobo/direction/views.py
@@ -1,60 +1,70 @@
+"""This is views file for MVT model"""
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
+
 from .carte.direction.vent import *
 
 from .views_function import function_map
 from .views_function import function_map2
 from .views_function import function_map3
-
 from .views_function import function_tchat
+
 from .data_tchat.database import tchat
 
+
+
 def navebarre_vent(request):
+    """This menu of wind function"""
     return render(request, "menu/navebarre_vent.html")
 
+
 def map1(request):
-    
+    """This is template for tchat map"""
+
     if request.method == "POST":
-        
+
         data = request.POST.get('text')
         out = function_tchat(data)
         return HttpResponse(out)
 
     message = tchat()
 
-    
+
     return render(request, "map1.html", {"message":message})
 
 @ensure_csrf_cookie
 def map(request):
+    """This is map template"""
 
     if request.method == "POST":
-        
- 
+
         data = request.POST.get('data')
         data2 = request.POST.get('a')
 
         if data:
             nouvelle_position, lat, long = function_map(data)
-   
-            return HttpResponse((lat,' ', long, ' ', nouvelle_position))
+
+            return HttpResponse((lat, ' ', long, ' ', nouvelle_position))
 
         if data2:
 
             lat, long, listee, index = function_map2(data2)     
-            nouvelle_position, lat, long = function_map3(lat, long, listee, index, data)
-                    
-            return HttpResponse((lat,' ', long, ' ', nouvelle_position))
+            nouvelle_position, lat, long = function_map3(lat,
+                                                         long,
+                                                         listee,
+                                                         index,
+                                                         data)
+
+            return HttpResponse((lat, ' ', long, ' ', nouvelle_position))
 
 
     return render(request, "map.html")
 
 
 
+if __name__ == "__main__":
+    """runner function need __name__ == "__main__" """
 
-if __name__=="__main__":
     map(request)
-    essais(request)
-
-    
