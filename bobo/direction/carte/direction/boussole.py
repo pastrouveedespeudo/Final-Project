91f7bd1e922fa5a927031671459325cb5ad794cd
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-13 22:32:39 +0200

    Update boussole.py
    5.78

--- bobo/direction/carte/direction/boussole.py
+++ bobo/direction/carte/direction/boussole.py
@@ -1,98 +1,73 @@
 """This is our tool direction"""
 
+
 def calcul_vent(direction):
     """We using trigonometric circle
        for determinate orientation of wind
        we transform degrees to str"""
-    
+
     if direction == 0 or direction == 360:
         return 'nord'
 
+    if direction > 0 and direction <= 22.5:
+        return 'nordnordest'
 
-    elif direction > 0 and direction <= 22.5:
-      
+    if direction > 22.5 and direction <= 45:
         return 'nordnordest'
-    
-    elif direction > 22.5 and direction <= 45:
-      
-        return 'nordnordest'
-    
-    elif direction == 45:
-     
+
+    if direction == 45:
         return 'nordest'
 
-    
-    elif direction > 45 and direction <= 67.5:
-     
+    if direction > 45 and direction <= 67.5:
         return 'estnordest'
-    
-    elif direction > 67.5 and direction <= 90:
-  
+
+    if direction > 67.5 and direction <= 90:
         return 'estnordest'
-    
-    elif direction > 90 and direction <= 112.5:
-        
+
+    if direction > 90 and direction <= 112.5:
         return 'est'
-    
-    elif direction > 112.5 and direction <= 135:
-     
+
+    if direction > 112.5 and direction <= 135:
         return 'estsudest'
-    
-    elif direction == 135:
-        
+
+    if direction == 135:
         return 'sudest'
 
-    
-    elif direction > 135 and direction <= 157.5:
-       
+    if direction > 135 and direction <= 157.5:
         return 'sudsudest'
 
-    
-    elif direction > 157.5 and direction <= 180:
-      
+    if direction > 157.5 and direction <= 180:
         return 'sudsudest'
 
-    
-    elif direction == 180:
-      
-         return 'sud'
-         
-    elif direction > 180 and direction <= 202.5:
-    
+    if direction == 180:
+        return 'sud'
+
+    if direction > 180 and direction <= 202.5:
         return 'sudsudouest'
 
-    
-    elif direction > 202.5 and direction <= 225:
-     
+    if direction > 202.5 and direction <= 225:
         return 'sudsudouest'
-    
-    elif direction == 225:
-        
+
+    if direction == 225:
         return 'sudouest'
 
-    elif direction > 225 and direction <= 247.5:
-       
+    if direction > 225 and direction <= 247.5:
         return 'sudouest'
-    
-    elif direction > 247.5 and direction <= 270:
-     
+
+    if direction > 247.5 and direction <= 270:
         return 'ouestsudouest'
-    
-    elif direction > 270:
-      
-        return 'ouest'
-    
-    elif direction > 270 and direction <= 292.5:
-      
-        return 'ouestnordouest'
-    
-    elif direction > 292.5 and direction <= 315:
-  
-        return 'nordouest'
-    
-    elif direction == 315:
 
+    if direction > 270:
+        return 'ouest'
+
+    if direction > 270 and direction <= 292.5:
+        return 'ouestnordouest'
+
+    if direction > 292.5 and direction <= 315:
         return 'nordouest'
-    
-    elif direction > 315 and direction <= 337.5:
+
+    if direction == 315:
+        return 'nordouest'
+
+    if direction > 315 and direction <= 337.5:
         return 'nordnordouest'
