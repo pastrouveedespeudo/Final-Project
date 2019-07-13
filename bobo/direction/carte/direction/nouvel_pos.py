b56555c5a602194f0572c7a98f9f4517ddaf31c0
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-13 22:40:58 +0200

    Update nouvel_pos.py
    9.20/10

--- bobo/direction/carte/direction/nouvel_pos.py
+++ bobo/direction/carte/direction/nouvel_pos.py
@@ -1,10 +1,6 @@
 """Here we calculate the new
-   direction"""
-
-from math import *
-
-
-"""We have calculate
+   direction
+   We have calculate
    a 1 degree = 111.11 km
    so we *0.009 the number of
    km for have degrees to km and add it to lat
@@ -12,27 +8,29 @@ from math import *
    east or west we * by degrees
    of circle trigo"""
 
+from math import cos
+from math import radians
 
 
-def long_lat(lat, long, km, sens):
+def long_lat(lat, long, kiloms, sens):
     """We calculate new direction"""
 
     #In case where superficie.py
     #fail or an exception is present
-    
+
     try:
-        km = float(km)
+        kiloms = float(kiloms)
     except:
-        km = 20.0
-        km = float(km)
+        kiloms = 20.0
+        kiloms = float(kiloms)
 
-    if km > 500:
-        km = 20.0
+    if kiloms > 500:
+        kiloms = 20.0
 
-    print(km, sens)
-    
-    kilometre = km * 0.009
-    
+    print(kiloms, sens)
+
+    kilometre = kiloms * 0.009
+
 
     if sens == 'sud':
         lat1 = kilometre
@@ -48,7 +46,7 @@ def long_lat(lat, long, km, sens):
         lat = lat + kilometre
         long = long + kilometre * cos(radians(45))
         return lat, long
-        
+
     elif sens == 'ouestsudouest':
         lat = lat + kilometre
         long = long + kilometre * cos(radians(22.5))
@@ -57,17 +55,17 @@ def long_lat(lat, long, km, sens):
     elif sens == 'ouest':
         long = long + kilometre
         return lat, long
-        
+
     elif sens == 'ouestnordouest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(157.5))
         return lat, long
-        
+
     elif sens == 'nordouest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(135))
         return lat, long
-        
+
     elif sens == 'nordnordouest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(112.5))
@@ -87,7 +85,7 @@ def long_lat(lat, long, km, sens):
         lat = lat - kilometre
         long = long - kilometre * cos(radians(315))
         return lat, long
-        
+
     elif sens == 'estnordest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(292.5))
@@ -96,21 +94,19 @@ def long_lat(lat, long, km, sens):
     elif sens == 'est':
         kilo = kilometre
         long1 = long - kilo
-      
         return lat, long1
 
     elif sens == 'estsudest':
         lat = lat + kilometre
         long = long - kilometre * cos(radians(337))
         return lat, long
-        
+
     elif sens == 'sudest':
         lat = lat + kilometre
         long = long - kilometre * cos(radians(315))
         return lat, long
-        
+
     elif sens == 'sudsudest':
         lat = lat + kilometre
         long = long - kilometre * cos(radians(292.5))
         return lat, long
-
