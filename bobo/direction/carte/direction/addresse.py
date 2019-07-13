127fccd02f9bbe63bc65379826dd08da080730bf
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-13 22:25:07 +0200

    Update addresse.py

--- bobo/direction/carte/direction/addresse.py
+++ bobo/direction/carte/direction/addresse.py
@@ -1,31 +1,38 @@
 """Here we search latitude and longititude
 from a city"""
 
-import reverse_geocoder as rg 
-import pprint 
-  
+
+import reverse_geocoder as rg
+
 def reverseGeocode(coordinates):
     """We reverse from have an address
        and not coordinates"""
-    
-    result = rg.search(coordinates) 
+
+    result = rg.search(coordinates)
     return result
 
 
 def dress_to_ville(lat, long):
     """We reverse from have an address
        and not coordinates (second technic)"""
-    coordinates =(lat, long) 
-    a = reverseGeocode(coordinates) 
+
+    coordinates = (lat, long)
+    revers = reverseGeocode(coordinates)
+
     liste = []
-    c = 0
-    for i,j in a[0].items():
-        if c == 2:
-            liste.append(j)
-        elif c == 3:
-            liste.append(j)
-        c+=1
+    counter = 0
 
-    ad = str(liste[0]+'+'+liste[1])
+    for _, item_deux in revers[0].items():
+        if counter == 2:
+            liste.append(item_deux)
 
-    return liste[0], ad
+        elif counter == 3:
+            liste.append(item_deux)
+
+        counter += 1
+
+    out = str(liste[0]+'+'+liste[1])
+
+    return liste[0], out
+
+#6.45/10
