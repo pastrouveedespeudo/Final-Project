9b91b00910646009e757ecd0029ebff8b9ac6f31
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-15 23:37:56 +0200

    Update views.py
    9/10 jpe pas plus serieux j'y suis pas arrivfé le dico, c ca de coder comme un iench tin y'en a trop

--- bobo/polution/views.py
+++ bobo/polution/views.py
@@ -1,28 +1,45 @@
 """Views from MVT model"""
 
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-import requests
-import urllib.request
-import datetime
-from bs4 import *
+from .graph.graphic_plugs import diagram_plugs
+from .graph.graphic_hour import diagram_hour
+from .graph.graphic_demonstration import diagram_demonstration
+from .graph.graphic_weather import diagram_weather
+from .graph.graphic_pressure import diagram_pressure
+from .graph.graphic_season import diagram_season
+from .graph.graphic_traffic import diagram_traffic
+from .graph.graphic_wind import diagram_wind
+from .graph.graphic_weekend import diagram_weekend
+from .graph.graphic_population import diagram_population
 
-from .graph.graphic_plugs import *
-from .graph.graphic_climate import *
-from .graph.graphic_hour import *
-from .graph.graphic_demonstration import *
-from .graph.graphic_weather import *
-from .graph.graphic_pressure import *
-from .graph.graphic_season import *
-from .graph.graphic_traffic import *
-from .graph.graphic_wind import *
-from .graph.graphic_weekend import *
-from .graph.graphic_population import *
+from .graph.graphic_traffic import visu_traffic
+from .graph.graphic_traffic import treatment_traffic
+from .graph.graphic_demonstration import visu_demonstration
+from .graph.graphic_demonstration import treatement_demonstration
+from .graph.graphic_hour import treatment_hour
+from .graph.graphic_plugs import visu_plugs
+from .graph.graphic_plugs import treatment_plugs
+from .graph.graphic_population import visu_population
+from .graph.graphic_population import treatement_population
+from .graph.graphic_pressure import visu_pressure
+from .graph.graphic_pressure import treatment_pressure
+from .graph.graphic_season import visu_season
+from .graph.graphic_season import treatment_season
+from .graph.graphic_weather import visu_weater
+from .graph.graphic_weather import treatement_weather
+from .graph.graphic_weekend import visu_weekend
+from .graph.graphic_weekend import treatment_weekend
+from .graph.graphic_wind import visu_wind
+from .graph.graphic_wind import treatment_wind
 
-
-from .prediction_site.analysa2 import *
+from .prediction_site.analysa2 import predi_analysa2
 
 from .views_function import data_function_particle
 from .views_function import data_function_weather
@@ -87,39 +104,38 @@ def tchat_polution_v(request):
     """Tchat pollution"""
 
     if request.method == "POST":
-        
+
         data = request.POST.get('text')
         out = function_tchat(data, 'tchat_polution')
         return HttpResponse(out)
 
     message = tchat_polution()
 
-    
     return render(request, "tchat_polution.html", {"message":message})
 
 
 
 def tchat_graphe_v(request):
     """Tchat graphe"""
-    
+
     if request.method == "POST":
-        
+
         data = request.POST.get('text')
         out = function_tchat(data, 'tchat_graphe')
         return HttpResponse(out)
 
     message = tchat_graphe()
 
-    
+
     return render(request, "tchat_graphe.html", {"message":message})
 
 
 
-def tchat_donnée_v(request):
+def tchat_donnee_v(request):
     """Tchat data"""
-    
+
     if request.method == "POST":
-        
+
         data = request.POST.get('text')
         out = function_tchat(data, 'tchat_donnée')
         return HttpResponse(out)
@@ -131,46 +147,46 @@ def tchat_donnée_v(request):
 
 def tchat_machine_a_o_v(request):
     """Tchat soluce"""
-    
+
     if request.method == "POST":
-        
+
         data = request.POST.get('text')
         out = function_tchat(data, 'tchat_machine_a_o')
         return HttpResponse(out)
 
     message = tchat_machine_a_o()
 
-    
+
     return render(request, "tchat_machine_a_o.html", {"message":message})
 
 
 def tchat_prediction_v(request):
     """Tchat prediction"""
-    
+
     if request.method == "POST":
-        
+
         data = request.POST.get('text')
         out = function_tchat(data, 'tchat_prediction')
         return HttpResponse(out)
 
     message = tchat_prediction()
 
-    
+
     return render(request, "tchat_prediction.html", {"message":message})
 
 
 def tchat_info_pollu_v(request):
     """Tchat information"""
-    
+
     if request.method == "POST":
-        
+
         data = request.POST.get('text')
         out = function_tchat(data, 'tchat_info_pollu')
         return HttpResponse(out)
 
     message = tchat_info_pollu()
 
-    
+
     return render(request, "tchat_info_pollu.html", {"message":message})
 
 
@@ -179,11 +195,26 @@ def polution(request):
     return render(request, 'polution.html')
 
 
-def donnée(request):
-    """Here we call all API recup data, and display it on HTML page
-    We need exception in case there are error from ur script (for call API)"""
-    
-    particle =  data_function_particle()
+def function_donnee_pep1():
+    """Pep8 function for donnee"""
+
+    socio = function_data_socio()
+    plugs = function_data_plugs()
+    erup = function_data_erup()
+    dollars = function_data_dollars()
+    fire = function_data_fire()
+    fertilizer = function_data_ferti()
+    periode = function_data_periode()
+    pole = data_function_pole()
+
+
+    return socio, plugs, erup, dollars,\
+           fire, fertilizer, periode, pole
+
+def function_donnee_pep():
+    """Pep8 function for donnee"""
+
+    particles = data_function_particle()
     weather = data_function_weather()
     wind = data_function_wind()
     temperature = data_function_temperature()
@@ -193,21 +224,23 @@ def donnée(request):
     rank = data_function_ranking()
     pressure = data_function_pressure()
     demonstration = data_function_demonstration()
-    socio = function_data_socio()
-    plugs = function_data_plugs()
-    erup = function_data_erup()
-    dollars = function_data_dollars()
-    fire = function_data_fire()
-    fertilizer = function_data_ferti()
-    periode = function_data_periode()
-    print(periode)
-    pole = data_function_pole()
 
+    return particles, weather, wind, temperature, season, deaparture,\
+           day, rank, pressure, demonstration
 
-    
-    return render(request, 'donnée.html', {'lyon':particle[0],
-                                           'paris':particle[1],
-                                           'marseille':particle[2],
+def donnée(request):
+    """Here we call all API recup data, and display it on HTML page
+    We need exception in case there are error from ur script (for call API)"""
+
+    particles, weather, wind, temperature, season, deaparture,\
+    day, rank, pressure, demonstration = function_donnee_pep()
+
+    socio, plugs, erup, dollars, pole,\
+    fire, fertilizer, periode = function_donnee_pep1()
+
+    return render(request, 'donnée.html', {'lyon':particles[0],
+                                           'paris':particles[1],
+                                           'marseille':particles[2],
                                            'weather_lyon':weather[0],
                                            'weather_marseille':weather[1],
                                            'weather_paris':weather[2],
@@ -261,7 +294,7 @@ def donnée(request):
                                            'po_paris':periode[2],
                                            'po_marseille':periode[3]})
 
-                        
+
     return render(request, 'donnée.html')
 
 def info_pollu(request):
@@ -271,7 +304,7 @@ def info_pollu(request):
 
 def machine_a_o(request):
     """Information page about Water machin"""
-    
+
     return render(request, 'machine_a_o.html')
 
 @ensure_csrf_cookie
@@ -281,31 +314,74 @@ def prediction(request):
     from prediction_site and try to match by condition
     with analysa2.py and return it in html page"""
 
-    
+
     if request.method == "POST":
-                                
-            city1 = request.POST.get('lyon')    
-            city2 = request.POST.get('paris')
-            city3 = request.POST.get('marseille')
 
-            if city1:
-                #from predi_site.analysa2.py
-                predi = predi_analysa2('lyon')
-                return HttpResponse(predi)
+        city1 = request.POST.get('lyon')
+        city2 = request.POST.get('paris')
+        city3 = request.POST.get('marseille')
 
-            if city2:
-                predi = predi_analysa2('paris')
-                return HttpResponse(predi)
+        if city1:
+            #from predi_site.analysa2.py
+            predi = predi_analysa2('lyon')
+            return HttpResponse(predi)
 
-            if city3:
-                predi = predi_analysa2('marseille')
-                return HttpResponse(predi)
-                
+        if city2:
+            predi = predi_analysa2('paris')
+            return HttpResponse(predi)
+
+        if city3:
+            predi = predi_analysa2('marseille')
+            return HttpResponse(predi)
+
     return render(request, 'prediction.html')
 
 
 
+def graphe_function(city, graph):
+    """Graphe function for pep8"""
 
+    the_graphe = ''
+
+    if graph == 'season':
+        graph_n = visu_season(city)
+        data = treatment_season(graph_n)
+        graph_o = diagram_season(data[0], data[1], data[2], data[3], data[4],
+                                 data[5], data[6], data[7],
+                                 'diagramme.png')
+        the_graphe = graph_o
+
+    if graph == 'wind':
+        graph_p = visu_wind(city)
+        data = treatment_wind(graph_p)
+        graph_q = diagram_wind(data[0], data[1], data[2], data[3], data[4],
+                               data[5], data[6], data[7], 'diagramme.png')
+        the_graphe = graph_q
+
+    if graph == 'weekend':
+        graph_r = visu_weekend(city)
+        data = treatment_weekend(graph_r)
+        graph_s = diagram_weekend(data[0], data[1], data[2], data[3],
+                                  'diagramme.png')
+        the_graphe = graph_s
+
+    if graph == 'traffic':
+        graph_t = visu_traffic(city)
+        data = treatment_traffic(graph_t)
+        graph_u = diagram_traffic(data[0], data[1], data[2], data[3],
+                                  'diagramme.png')
+        the_graphe = graph_u
+
+    if graph == 'pressure':
+        graph_l = visu_pressure(city)
+        data = treatment_pressure(graph_l)
+        graph_m = diagram_pressure(data[0], data[1], data[2], data[3], data[4],
+                                   data[5], 'diagramme.png')
+        the_graphe = graph_m
+
+
+
+    return HttpResponse(the_graphe)
 
 def graphe(request):
     """Graphic page. Here we ask database by Ajax call
@@ -314,138 +390,69 @@ def graphe(request):
     we make an average and a variance by fonction_graphe from graphe file
     and display it by graphe function with matplotlib"""
 
-    #Ajax call
     if request.method == "POST":
-        
-        #We recup city
-        #And the condition
-        #For example Lyon condition plugs
-        city = request.POST.get('city')    
-        graph = request.POST.get('graph')  
-                                            
+
+        city = request.POST.get('city')
+        graph = request.POST.get('graph')
+
         print(city, graph)
 
-        #We ask database
-        #We trait it
+        the_graphe = ''
+
         if graph == 'plugs':
-            a = visu_plugs(city)       
-            data = treatment_plugs(a)
-            #and make a visual by matplolib
-            b = diagram_plugs(data[0], data[1], data[2], data[3], data[4],
-                      data[5], data[6], data[7], data[8], data[9],
-                      data[10], data[11], 'diagramme.png')  
-        
-            #and return a HttpResponse, here the graph
-            return HttpResponse(b)
+            graph_a = visu_plugs(city)
+            data = treatment_plugs(graph_a)
+            graph_b = diagram_plugs(data[0], data[1], data[2], data[3], data[4],
+                                    data[5], data[6], data[7], data[8], data[9],
+                                    data[10], data[11], 'diagramme.png')
+
+            the_graphe = graph_b
 
 
-
-        elif graph == 'hour':
+        if graph == 'hour':
             schedule = treatment_hour(city)
-            e = diagram_hour(schedule[0], schedule[1], schedule[2], schedule[3],
-                            'diagramme.png')
-       
-            return HttpResponse(e)
+            graph_e = diagram_hour(schedule[0], schedule[1], schedule[2], schedule[3],
+                                   'diagramme.png')
+            the_graphe = graph_e
 
 
+        if graph == 'demonstration':
+            graph_f = visu_demonstration(city)
+            data = treatement_demonstration(graph_f)
+            graph_g = diagram_demonstration(data[0], data[1],
+                                            data[2], data[3], 'diagramme.png')
+            the_graphe = graph_g
 
-        elif graph == 'demonstration':
-            f = visu_demonstration(city)
-            data = treatement_demonstration(f)
-            g = diagram_demonstration(data[0], data[1],
-                    data[2], data[3], 'diagramme.png')
-
-            
-            return HttpResponse(g)
-
-
-
-
-        elif graph == 'weather':
-            h = visu_weater(city)
-            data = treatement_weather(h)
-            i = diagram_weather(data[0], data[1], data[2], data[3], data[4],
-                      data[5], 'diagramme.png')
-            
-           
-            return HttpResponse(i)
+        if graph == 'weather':
+            graph_h = visu_weater(city)
+            data = treatement_weather(graph_h)
+            graph_i = diagram_weather(data[0], data[1], data[2], data[3], data[4],
+                                      data[5], 'diagramme.png')
+            the_graphe = graph_i
 
 
-
-                        
-        elif graph == 'population':
-            j = visu_population()
-            data = treatement_population(j)
-            k = diagram_population(data[0], data[1], data[2], data[3], data[4],
-                      data[5], 'diagramme.png')
-           
-            return HttpResponse(k)
-
-        
-        elif graph == 'pressure':
-            l = visu_pressure(city)
-            data = treatment_pressure(l)
-            m = diagram_pressure(data[0], data[1], data[2], data[3], data[4],
-                      data[5], 'diagramme.png')
-
-         
-            return HttpResponse(m)
+        if graph == 'population':
+            graph_j = visu_population()
+            data = treatement_population(graph_j)
+            graph_k = diagram_population(data[0], data[1], data[2], data[3], data[4],
+                                         data[5], 'diagramme.png')
+            the_graphe = graph_k
 
 
+        else:
+            the_graphe = graphe_function(city, graph)
 
 
-    
-        elif graph == 'season':
-            n = visu_season(city)
-            data = treatment_season(n)
+        return HttpResponse(the_graphe)
 
-            o = diagram_season(data[0], data[1], data[2], data[3], data[4],
-                      data[5], data[6], data[7],
-                             'diagramme.png')
-        
-            return HttpResponse(o)
-
-
-            
-        elif graph == 'wind':
-            p = visu_wind(city)
-            data = treatment_wind(p)
-            q = diagram_wind(data[0], data[1], data[2], data[3], data[4],
-                      data[5], data[6], data[7],'diagramme.png')
-
-            
-            return HttpResponse(q)
-
-        
-        elif graph == 'weekend':
-
-            r = visu_weekend(city)
-            data = treatment_weekend(r)
-            s = diagram_weekend(data[0], data[1], data[2], data[3],
-                              'diagramme.png')
-         
-            return HttpResponse(s)
-
-        
-        elif graph == 'traffic':
-            t = visu_traffic(city)
-            data = treatment_traffic(t)
-            u = diagram_traffic(data[0], data[1], data[2], data[3],
-                                'diagramme.png')
-   
-            return HttpResponse(u)
-  
-
-        
     return render(request, 'graphe.html')
 
 
 
-
 def hour():
+    """Here we define the hour"""
 
     date = datetime.datetime.now()
-
     hours = date.hour
     minute = date.minute
 
@@ -453,19 +460,15 @@ def hour():
 
 
 
-
-
 def temps(lieu):
     """We recup weather from api openweathermap"""
-    
-    clé = '5a72ceae1feda40543d5844b2e04a205'
-        
-    localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}".format(lieu,clé)
 
-    r = requests.get(localisation)
+    key = '5a72ceae1feda40543d5844b2e04a205'
+    localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}"
+    localisation = localisation.format(lieu, key)
+    request_html = requests.get(localisation)
+    data = request_html.json()
 
-    data=r.json()
-    
     weather = data['weather'][0]['main']
 
     if weather == "Clear":
@@ -480,11 +483,8 @@ def particle(lieu):
     """particle stuff we recup particle from airplumair"""
 
     path = "https://air.plumelabs.com/fr/live/{}".format(lieu)
-
-    r = requests.get(path)
-
-
-    page = r.content
+    request_html = requests.get(path)
+    page = request_html.content
     soup = BeautifulSoup(page, "html.parser")
 
     liste = []
@@ -492,30 +492,8 @@ def particle(lieu):
     for i in propriete:
         liste.append(i.get_text())
 
-    sentence = "a atteint un niveau élevé de pollution. Supérieur à la limite maximum pour 24h établie par l'OMS"
-    
-    search = str(liste).find(str(sentence))
+
     liste_e = liste[20:21]
     pollute = liste_e[0][31:34]
 
     return pollute
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
