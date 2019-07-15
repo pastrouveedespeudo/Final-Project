fff2c5806cc83d04796dbaeda657fd4b9bf41f30
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 01:05:44 +0200

    Update analysa2.py

--- bobo/polution/prediction_site/analysa2.py
+++ bobo/polution/prediction_site/analysa2.py
@@ -1,26 +1,68 @@
 """We matching with database"""
 
-import psycopg2
+from statistics import mean
 import datetime
-from statistics import *
+import psycopg2
 
-from .angrais import *
-from .climat import *
-from .diesel import *
-from .eruption import *
-from .incendie import *
-from .jour_nuit import *
-from .météo import *
-from .particule import *
-from .polenne import *
-from .socio import *
-from .trafique import *
+from .angrais import periode_angrais
 
-from .aide_analysa2 import *
+from .climat import saison
+from .climat import recuperation_donnée_température
+
+from .diesel import recup_tag
+from .eruption import eruption
+from .incendie import incendie
+from .jour_nuit import nuit_jour
+
+from .météo import pression
+from .météo import vent
+from .météo import recuperation_donnée_météo
+
+from .particule import particule2
+
+from .polenne import polenne
+from .socio import habitant
+
+from .trafique import trafique_circulation
+from .trafique import heure_de_pointe
+from .trafique import habitude
+from .trafique import bouchons
+from .trafique import activité_execptionnelle
+
+from .aide_analysa2 import vision
+
+def function_predi(condition, i):
+    """This is function for pep8"""
+
+    data_a = periode_angrais()
+    condition.append(data_a)
+
+    data_c = saison()
+    condition.append(data_c)
+
+    data_d = recup_tag()
+    condition.append(data_d)
+
+    data_e = eruption()
+    condition.append(data_e)
+
+    data_g = nuit_jour()
+    condition.append(data_g)
+
+    data_r = trafique_circulation()
+    condition.append(data_r)
+
+    data_s = heure_de_pointe()
+    condition.append(data_s)
+
+    data_t = habitude()
+    condition.append(data_t)
+
+    data_u = bouchons(i)
+    condition.append(data_u)
 
 
-
-def predi_analysa2(ville):
+def predi_analysa(ville):
     """We insert data into a list
     and compare it with database
     we mean it
@@ -30,80 +72,61 @@ def predi_analysa2(ville):
 
     condition = []
     for i in liste:
-        
-        m = particule2(i)
-        print('taux de particule de', m, 'a', i)
 
+        data_m = particule2(i)
+        print('taux de particule de', data_m, 'a', i)
 
-        a = periode_angrais()
-        condition.append(a)
-       
-        c = saison()
-        condition.append(c)
-       
-        d = recup_tag()
-        condition.append(d)
-      
-        e = eruption()
-        condition.append(e)
-     
-        g = nuit_jour()
-        condition.append(g)
-      
-        r = trafique_circulation()
-        condition.append(r)
-     
-        s = heure_de_pointe()
-        condition.append(s)
-        
-        t = habitude()
-        condition.append(t)
-     
-        u = bouchons(i)
-        condition.append(u)
-     
-        v = activité_execptionnelle(i)
-        condition.append(v)
+        function_predi(condition, i)
 
-        q = habitant(i)
-        condition.append(q)
-      
+        data_v = activité_execptionnelle(i)
+        condition.append(data_v)
+
+        data_q = habitant(i)
+        condition.append(data_q)
+
 ##        n = industrie(i)
 ##        condition.append(n)
 
-        o = polenne(i)
-        condition.append(o)
+        data_o = polenne(i)
+        condition.append(data_o)
 
 ##        l = france(i)
 ##        condition.append(l)
-      
-        h = recuperation_donnée_météo(i)
-        condition.append(h)
-        
-        j = vent(i)
-        condition.append(j)
-        
-        k = pression(i)
-        condition.append(k)
-        
-        b = recuperation_donnée_température(i)
-        condition.append(b)
-     
-        f = incendie(i)
-        condition.append(f)
 
-        
+        data_h = recuperation_donnée_météo(i)
+        condition.append(data_h)
+
+        data_j = vent(i)
+        condition.append(data_j)
+
+        data_k = pression(i)
+        condition.append(data_k)
+
+        data_b = recuperation_donnée_température(i)
+        condition.append(data_b)
+
+        data_f = incendie(i)
+        condition.append(data_f)
+
     print(condition)
+    return condition
+
+
+
+def predi_analysa2(ville):
+    """We compare it now"""
+
+    condition = predi_analysa(ville)
     les_conditions = vision(ville)
 
     same = []
     for i in les_conditions:
-        c = 0
+        counter = 0
         diff = 0
         for j in i[:-1]:
-            if j != condition[c]:
+            if j != condition[counter]:
                 diff += 1
-            c+=1
+            counter += 1
         if diff == 0:
             same.append(i[-1])
 
@@ -118,10 +141,11 @@ def predi_analysa2(ville):
     for i in liste_pol:
         print(i)
 
-    try:
+    if liste_pol == []:
+        moyenne = 'pas de donnée'
+    else:
         moyenne = mean(liste_pol)
         print(moyenne)
-    except:
-        moyenne = 'pas de donnée'
+
 
     return moyenne
