b220d1fba5925040b8a7659b2912092d0b1f35ec
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 01:14:34 +0200

    Update angrais.py

--- bobo/polution/prediction_site/angrais.py
+++ bobo/polution/prediction_site/angrais.py
@@ -1,23 +1,16 @@
 """We recup fertilizer data for site web"""
 
-import requests
-import datetime
-import urllib.request
-from bs4 import *
 import datetime
 
 def periode_angrais():
+    """We define the month and match it with fertilizer period"""
 
-    mois_angrais = [5,6]
-
+    mois_angrais = [5, 6]
 
     date = datetime.datetime.now()
-    
-    jour = date.day
     mois = date.month
-    année = date.year
 
     for i in mois_angrais:
         if mois == i:
             return 'oui'
-            break
+    return None
