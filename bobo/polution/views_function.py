af2dfbe5fc471c3871bc80afec123fbc44365179
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-15 22:38:19 +0200

    Update views_function.py
    enfete les except je crois que sté pour me rassurer...

--- bobo/polution/views_function.py
+++ bobo/polution/views_function.py
@@ -1,7 +1,18 @@
 """This views function here we lighter
     the views"""
 
-from .data_site.pollution import *
+from .data_site.pollution import particle_rate
+from .data_site.pollution import pressure_city
+from .data_site.pollution import weather_city
+from .data_site.pollution import climate_city
+from .data_site.pollution import season
+from .data_site.pollution import habit
+from .data_site.pollution import city_ranking_pollute
+from .data_site.pollution import industrial_area
+from .data_site.pollution import exceptional_activity
+from .data_site.pollution import socio
+from .data_site.pollution import plugs
+
 from .data_site.fertilizer import period_fertilizer
 from .data_site.diesel import recup_tag, course_dollars
 from .data_site.eruption import eruption
@@ -45,7 +56,7 @@ def function_tchat(data, tchat):
         database_prediction(data)
     elif tchat == "tchat_info_pollu":
         database_info_pollu(data)
-    
+
     return data
 
 
@@ -59,7 +70,7 @@ def data_function_particle():
     except:
         data_lyon = "No data"
         data_paris = "No data"
-        data_marseille = "No data" 
+        data_marseille = "No data"
 
     return data_lyon, data_paris, data_marseille
 
@@ -85,28 +96,27 @@ def data_function_weather():
 
 def data_function_wind():
     """We recup wind of cites"""
-    
+
     try:
         wind_lyon = weather_city('lyon', 'vent')
         wind_paris = weather_city('paris', 'vent')
-        wind_marseille = weather_city('marseille', 'vent') 
+        wind_marseille = weather_city('marseille', 'vent')
     except:
-     
         wind_lyon = "No data"
         wind_paris = "No data"
-        wind_marseille = "No data"  
+        wind_marseille = "No data"
 
     return wind_lyon, wind_paris, wind_marseille
 
 
-    
+
 def data_function_temperature():
     """We recup temperate of cites"""
-    
+
     try:
         temperature_lyon = climate_city('lyon')
         temperature_paris = climate_city('paris')
-        temperature_marseille = climate_city('marseille') 
+        temperature_marseille = climate_city('marseille')
     except:
         temperature_lyon = "No data"
         temperature_paris = "No data"
@@ -118,26 +128,26 @@ def data_function_temperature():
 
 def data_function_season():
     """We recup season of cites"""
-    
+
     try:
-        current_season =  season()
+        current_season = season()
     except:
         current_season = "No data"
-    
+
     return current_season
 
 
 
 def data_function_departure():
     """We recup deaparture of cites"""
-    
+
     no_point_lyon = ""
     departure = Traffic('lyon')
-    
+
     try:
-        departure_lyon = departure[0] 
+        departure_lyon = departure[0]
         hour_point_lyon = departure[1]
-        regular_day_lyon = departure[2] 
+        regular_day_lyon = departure[2]
 
         if hour_point_lyon == 'oui':
             no_point_lyon = 'non'
@@ -145,23 +155,21 @@ def data_function_departure():
 
         else:
             no_point_lyon = 'oui'
-            
+
     except:
         departure_lyon = "No data"
         hour_point_lyon = "No data"
         regular_day_lyon = "No data"
 
-        
+
 
     return departure_lyon, hour_point_lyon,\
            regular_day_lyon, no_point_lyon
-           
-
 
 
 def data_function_day():
     """We recup habit of person"""
-    
+
     try:
         habitt = habit()
         weekend = habitt[0]
@@ -178,7 +186,7 @@ def data_function_day():
 
 def data_function_ranking():
     """We recup ranking of cites"""
-    
+
     try:
         ranking_lyon = city_ranking_pollute('lyon')
         ranking_paris = city_ranking_pollute('paris')
@@ -196,7 +204,7 @@ def data_function_ranking():
 
 def data_function_pole():
     """We recup pole of cites"""
-    
+
     try:
         pole_lyon = industrial_area('lyon')
         pole_paris = industrial_area('paris')
@@ -214,11 +222,11 @@ def data_function_pole():
 
 def data_function_pressure():
     """We recup pressure of cites"""
-    
+
     try:
         pressure_lyon = pressure_city('lyon')
         pressure_paris = pressure_city('paris')
-        pressure_marseille = pressure_city('marseille') 
+        pressure_marseille = pressure_city('marseille')
 
     except:
         pressure_lyon = "No data"
@@ -232,12 +240,12 @@ def data_function_pressure():
 
 def data_function_demonstration():
     """We recup demonstration of cites"""
-    
+
     try:
         demonstration_lyon = exceptional_activity('lyon')
         demonstration_paris = exceptional_activity('paris')
         demonstration_marseille = exceptional_activity('marseille')
-        
+
     except:
         demonstration_lyon = "No data"
         demonstration_paris = "No data"
@@ -250,28 +258,27 @@ def data_function_demonstration():
 
 def function_data_socio():
     """We recup socio of cites"""
-    
+
     try:
         socio_lyon = socio('lyon')
         socio_paris = socio('paris')
         socio_marseille = socio('marseille')
-        
+
     except:
         socio_lyon = "No data"
         socio_paris = "No data"
         socio_marseille = "No data"
-        
+
     return socio_lyon, socio_paris,\
            socio_marseille
 
 
-
 def function_data_plugs():
     """We recup plugs of cites"""
-    
+
     try:
         plugs_lyon = plugs('lyon')
-        plugs_paris = plugs('paris') 
+        plugs_paris = plugs('paris')
 
     except:
         plugs_lyon = "No data"
@@ -282,7 +289,7 @@ def function_data_plugs():
 
 def function_data_erup():
     """We recup erruption of world"""
-    
+
     try:
         errup = eruption()
         if errup == 'oui':
@@ -297,7 +304,7 @@ def function_data_erup():
 
 def function_data_dollars():
     """We recup dollard tendance of cites"""
-    
+
     try:
         diesel = recup_tag()
         dollars = course_dollars()
@@ -312,22 +319,22 @@ def function_data_dollars():
 
 def function_data_fire():
     """We recup fire of cites"""
-    
+
     try:
         fire_lyon = fire_city('lyon')
-        
+
         if fire_lyon == 'oui':
             fire_lyon = 'oui'
         else:
             fire_lyon = 'non'
-        
+
         fire_marseille = fire_city('marseille')
-        
+
         if fire_marseille == 'oui':
             fire_marseille = 'oui'
         else:
             fire_marseille = 'non'
-            
+
         fire_paris = fire_city('paris')
         if fire_paris == 'oui':
             fire_paris = 'oui'
@@ -341,8 +348,6 @@ def function_data_fire():
     return fire_lyon, fire_marseille,\
            fire_paris
 
-
-
 def function_data_ferti():
     """We recup fertilizer of cites"""
 
@@ -352,8 +357,6 @@ def function_data_ferti():
         fertilizer = "No data"
 
     return fertilizer
-
-
 
 def function_data_periode():
     """We recup period of journey"""
