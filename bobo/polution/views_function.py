142f3759683563ff42b28c92f5fd6d7744ebedb4
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 23:32:00 +0200

    Update views_function.py

--- bobo/polution/views_function.py
+++ bobo/polution/views_function.py
@@ -1,29 +1,33 @@
-"""here we try to lighten the views in code
-    We trying or pass because
-    it's a function who calling the database
-    if the database is empty
-    it return an error
-    All of this
-    is content into static.bobo becase
-    they are functions
-    who trait picture"""
+"""This views function here we lighter
+    the views"""
+
+from .data_site.pollution import particle_rate
+from .data_site.pollution import pressure_city
+from .data_site.pollution import weather_city
+from .data_site.pollution import climate_city
+from .data_site.pollution import season
+from .data_site.pollution import habit
+from .data_site.pollution import city_ranking_pollute
+from .data_site.pollution import industrial_area
+from .data_site.pollution import exceptional_activity
+from .data_site.pollution import socio
+from .data_site.pollution import plugs
+
+from .data_site.fertilizer import period_fertilizer
+from .data_site.diesel import recup_tag, course_dollars
+from .data_site.eruption import eruption
+from .data_site.fire import fire_city
+from .data_site.day_night import night_day
+from .data_site.polenne import polenne
+from .data_site.pollution import traffic as Traffic
 
 
-import os
-
-from .tendance_file.analysis_database.tendance import la_tendance
-
-from .magasins.address import address_geo
-from .magasins.address import city_geo
-from .magasins.gym import big_city_gym
-from .magasins.gym import schedule_gym
-
-from .magasins.hairdresser import cities
-from .magasins.hairdresser import schedule_hair
-
-from .data_tchat.database import database_coupe
-from .data_tchat.database import database_habit
-from .data_tchat.database import database_tendance
+from .data_tchat.database import database_pollution
+from .data_tchat.database import database_graphe
+from .data_tchat.database import database_donnée
+from .data_tchat.database import database_machine_a_o
+from .data_tchat.database import database_prediction
+from .data_tchat.database import database_info_pollu
 
 from .data_tchat.CONFIG import GROS_MOTS
 
@@ -40,146 +44,333 @@ def function_tchat(data, tchat):
     if data == "":
         return "non"
 
-    if tchat == "tchat_coupe":
-        database_coupe(data)
-    elif tchat == "tchat_habit":
-        database_habit(data)
-    elif tchat == "tchat_tendance":
-        database_tendance(data)
+    if tchat == "tchat_polution":
+        database_pollution(data)
+    elif tchat == "tchat_graphe":
+        database_graphe(data)
+    elif tchat == "tchat_donnée":
+        database_donnée(data)
+    if tchat == "tchat_machine_a_o":
+        database_machine_a_o(data)
+    elif tchat == "tchat_prediction":
+        database_prediction(data)
+    elif tchat == "tchat_info_pollu":
+        database_info_pollu(data)
 
     return data
 
 
-def database_mode_function():
-    """This function for database"""
+def data_function_particle():
+    """We recup particle of cites"""
 
     try:
-        os.chdir('/app/static/bobo')
+        data_lyon = particle_rate('lyon')
+        data_paris = particle_rate('paris')
+        data_marseille = particle_rate('marseille')
     except:
-        os.chdir(r'C:\Users\jeanbaptiste\bobo\bobo\static\bobo')
+        data_lyon = "No data"
+        data_paris = "No data"
+        data_marseille = "No data"
 
-    liste = os.listdir()
-
-    liste1 = []
-    theliste1 = []
-
-    for i in liste:
-        theliste1.append(i)
-
-    theliste1 = sorted(theliste1)
-
-    counter = 0
-    for i in theliste1:
-        try:
-            liste1.append((str(theliste1[counter]),
-                           str(theliste1[counter + 1]),
-                           int(str(counter) + str(counter))))
-            counter += 2
-        except:
-            pass
-
-    return liste1
+    return data_lyon, data_paris, data_marseille
 
 
-def tendance_function():
-    """This is tendance function"""
+def data_function_weather():
+    """We recup weather of cites"""
 
-    liste10 = la_tendance()
-    return liste10
-
-
-def the_colors_function(color):
-    """This is color function we return colors"""
-
-    liste10 = la_tendance()
-
-    if color == 'blonde':
-        coul_analyse_haut = liste10[1][0]
-        coul_analyse_bas = liste10[1][1]
-
-    elif color in ('brune', 'noire'):
-        coul_analyse_haut = liste10[0][0]
-        coul_analyse_bas = liste10[0][1]
-
-    elif color in ('chatain', 'rousse'):
-
-        coul_analyse_haut = liste10[2][0]
-        coul_analyse_bas = liste10[2][1]
-
-    return coul_analyse_haut, coul_analyse_bas
-
-
-def gymm_map_function(gymm_map, gym_pays):
-    """here we retrieve the name of the
-    gym sought by the user and
-    seek his address"""
-
-    the_address = address_geo(gymm_map, gym_pays)
-    lat_long = city_geo(the_address)
-
-    out = ''
-
-    if lat_long == "Oups nous n'avons rien trouvé":
-        out = lat_long
-
-    else:
-        data = str(lat_long[0]) + ' ' + str(lat_long[1])
-        out = data
-    return out
-
-def gymm_function(gymm):
-    """This is gym function we return gym list"""
-
-    gym_list = []
-
-    the_cities = big_city_gym(gymm)
-
-    for i in the_cities:
-        if len(gym_list) == 4:
-            return gym_list
-
-        the_schedule = schedule_gym(i, gymm)
-
-        if the_schedule != []:
-            gym_list.append([i, the_schedule, ""])
-
-    return gym_list
+    try:
+        weather_lyon = weather_city('lyon', 'météo')
+        weather_paris = weather_city('paris', 'météo')
+        weather_marseille = weather_city('marseille', 'météo')
+    except:
+        weather_lyon = "No data"
+        weather_paris = "No data"
+        weather_marseille = "No data"
 
 
 
-def haircut_style_function(haircut_style):
-    """This is haircut_style_function function
-    we return hairdressers"""
+    return weather_lyon, weather_paris, weather_marseille
 
-    coif = []
 
-    the_hairdressers = cities(haircut_style)
 
-    for i in the_hairdressers:
-        schedule1 = schedule_hair(i, haircut_style)
 
-        if [schedule1] == [] or schedule1 == []\
-           or schedule1 == "" or schedule1 == " "\
-           or schedule1 is None:
-            the_hairdressers.remove(i)
+def data_function_wind():
+    """We recup wind of cites"""
+
+    try:
+        wind_lyon = weather_city('lyon', 'vent')
+        wind_paris = weather_city('paris', 'vent')
+        wind_marseille = weather_city('marseille', 'vent')
+    except:
+        wind_lyon = "No data"
+        wind_paris = "No data"
+        wind_marseille = "No data"
+
+    return wind_lyon, wind_paris, wind_marseille
+
+
+
+def data_function_temperature():
+    """We recup temperate of cites"""
+
+    try:
+        temperature_lyon = climate_city('lyon')
+        temperature_paris = climate_city('paris')
+        temperature_marseille = climate_city('marseille')
+    except:
+        temperature_lyon = "No data"
+        temperature_paris = "No data"
+        temperature_marseille = "No data"
+
+
+    return temperature_lyon, temperature_paris,\
+           temperature_marseille
+
+def data_function_season():
+    """We recup season of cites"""
+
+    try:
+        current_season = season()
+    except:
+        current_season = "No data"
+
+    return current_season
+
+
+
+def data_function_departure():
+    """We recup deaparture of cites"""
+
+    no_point_lyon = ""
+    departure = Traffic('lyon')
+
+    try:
+        departure_lyon = departure[0]
+        hour_point_lyon = departure[1]
+        regular_day_lyon = departure[2]
+
+        if hour_point_lyon == 'oui':
+            no_point_lyon = 'non'
+
 
         else:
-            coif.append([i, schedule1, ""])
-            the_hairdressers.remove(i)
+            no_point_lyon = 'oui'
 
-    return coif
+    except:
+        departure_lyon = "No data"
+        hour_point_lyon = "No data"
+        regular_day_lyon = "No data"
 
 
-def map_hairdresser_function(map_hairdresser, vivile):
-    """This is map_hairdresser_function function"""
 
-    the_address = address_geo(map_hairdresser, vivile)
+    return departure_lyon, hour_point_lyon,\
+           regular_day_lyon, no_point_lyon
 
-    lat_long = city_geo(the_address)
-    if lat_long == "Oups nous n'avons rien trouvé":
-        data = "Oups nous n'avons rien trouvé"
 
-    else:
-        data = str(lat_long[0]) + ' ' + str(lat_long[1])
+def data_function_day():
+    """We recup habit of person"""
 
-    return data
+    try:
+        habitt = habit()
+        weekend = habitt[0]
+        day = habitt[1]
+    except:
+        day = "No data"
+        weekend = "No data"
+
+
+    return weekend, day
+
+
+
+
+def data_function_ranking():
+    """We recup ranking of cites"""
+
+    try:
+        ranking_lyon = city_ranking_pollute('lyon')
+        ranking_paris = city_ranking_pollute('paris')
+        ranking_marseille = city_ranking_pollute('marseille')
+
+    except:
+        ranking_lyon = "No data"
+        ranking_paris = "No data"
+        ranking_marseille = "No data"
+
+    return ranking_lyon, ranking_paris,\
+           ranking_marseille
+
+
+
+def data_function_pole():
+    """We recup pole of cites"""
+
+    try:
+        pole_lyon = industrial_area('lyon')
+        pole_paris = industrial_area('paris')
+        pole_marseille = industrial_area('marseille')
+
+    except:
+        pole_lyon = "No data"
+        pole_paris = "No data"
+        pole_marseille = "No data"
+
+    return pole_lyon, pole_paris,\
+           pole_marseille
+
+
+
+def data_function_pressure():
+    """We recup pressure of cites"""
+
+    try:
+        pressure_lyon = pressure_city('lyon')
+        pressure_paris = pressure_city('paris')
+        pressure_marseille = pressure_city('marseille')
+
+    except:
+        pressure_lyon = "No data"
+        pressure_paris = "No data"
+        pressure_marseille = "No data"
+
+    return pressure_lyon, pressure_paris,\
+           pressure_marseille
+
+
+
+def data_function_demonstration():
+    """We recup demonstration of cites"""
+
+    try:
+        demonstration_lyon = exceptional_activity('lyon')
+        demonstration_paris = exceptional_activity('paris')
+        demonstration_marseille = exceptional_activity('marseille')
+
+    except:
+        demonstration_lyon = "No data"
+        demonstration_paris = "No data"
+        demonstration_marseille = "No data"
+
+    return demonstration_lyon, demonstration_paris,\
+           demonstration_marseille
+
+
+
+def function_data_socio():
+    """We recup socio of cites"""
+
+    try:
+        socio_lyon = socio('lyon')
+        socio_paris = socio('paris')
+        socio_marseille = socio('marseille')
+
+    except:
+        socio_lyon = "No data"
+        socio_paris = "No data"
+        socio_marseille = "No data"
+
+    return socio_lyon, socio_paris,\
+           socio_marseille
+
+
+def function_data_plugs():
+    """We recup plugs of cites"""
+
+    try:
+        plugs_lyon = plugs('lyon')
+        plugs_paris = plugs('paris')
+
+    except:
+        plugs_lyon = "No data"
+        plugs_paris = "No data"
+
+    return plugs_lyon, plugs_paris
+
+
+def function_data_erup():
+    """We recup erruption of world"""
+
+    try:
+        errup = eruption()
+        if errup == 'oui':
+            pass
+        else:
+            errup = 'non'
+    except:
+        errup = "No data"
+
+    return errup
+
+
+def function_data_dollars():
+    """We recup dollard tendance of cites"""
+
+    try:
+        diesel = recup_tag()
+        dollars = course_dollars()
+
+    except:
+        diesel = "No data"
+        dollars = "No data"
+
+    return diesel, dollars
+
+
+
+def function_data_fire():
+    """We recup fire of cites"""
+
+    try:
+        fire_lyon = fire_city('lyon')
+
+        if fire_lyon == 'oui':
+            fire_lyon = 'oui'
+        else:
+            fire_lyon = 'non'
+
+        fire_marseille = fire_city('marseille')
+
+        if fire_marseille == 'oui':
+            fire_marseille = 'oui'
+        else:
+            fire_marseille = 'non'
+
+        fire_paris = fire_city('paris')
+        if fire_paris == 'oui':
+            fire_paris = 'oui'
+        else:
+            fire_paris = 'non'
+    except:
+        fire_lyon = "No data"
+        fire_marseille = "No data"
+        fire_paris = "No data"
+
+    return fire_lyon, fire_marseille,\
+           fire_paris
+
+def function_data_ferti():
+    """We recup fertilizer of cites"""
+
+    try:
+        fertilizer = period_fertilizer()
+    except:
+        fertilizer = "No data"
+
+    return fertilizer
+
+def function_data_periode():
+    """We recup period of journey"""
+
+    try:
+        periode = night_day()
+        po_lyon = polenne('lyon')
+        po_marseille = polenne('marseille')
+        po_paris = polenne('paris')
+
+    except:
+        periode = "No data"
+        po_lyon = "No data"
+        po_marseille = "No data"
+        po_paris = "No data"
+
+    return periode, po_lyon, po_marseille, po_paris
