bfddd5fc2a4cc0897bfd45be92d8c9f3b2871621
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 15:55:46 +0200

    Update diesel.py

--- bobo/polution/prediction_site/diesel.py
+++ bobo/polution/prediction_site/diesel.py
@@ -1,46 +1,40 @@
 """We recup diesel data"""
 
 import requests
-import urllib.request
-from bs4 import *
+from bs4 import BeautifulSoup
 
 def course_dollars():
     """Here we try to get course of dollars BeautifulSoup and Request"""
-    
+
     path = "https://prixdubaril.com/"
-
-    r = requests.get(path)
-
-    page = r.content
+    request_html = requests.get(path)
+    page = request_html.content
     soup = BeautifulSoup(page, "html.parser")
-
     propriete = soup.find_all("span")
-
 
     liste = []
     liste.append(str(propriete))
 
-
     dollar = liste[0][520:525]
-    
-    if dollar[0] == '+':
-        return 'dollars augmente'
-    else:
-       return 'dollars baisse  '
-       #we let this space for visual aspect (for html page)
 
+    course = ''
+
+    if dollar[0] == '+':
+        course = 'dollars augmente'
+    else:
+        course = 'dollars baisse  '
+
+    return course
 
 
 def soup_function():
-    
-    #course_dollars()
-    dol = course_dollars()  
+    """Soup function we search all tags (carburant_red)"""
 
-    path = "https://prixdubaril.com/" 
-    r = requests.get(path)
-    page = r.content
-    soup = BeautifulSoup(page, "html.parser")   
-
+    dol = course_dollars()
+    path = "https://prixdubaril.com/"
+    request_html = requests.get(path)
+    page = request_html.content
+    soup = BeautifulSoup(page, "html.parser")
     propriete = soup.find_all("div", {'class':'carburant_red'})
 
     return propriete, dol
@@ -50,64 +44,77 @@ def finding_function():
     """Here we finding this words
     becasue we search red color
     it significate his increase"""
-    
-    #soup_function()
+
     propriete, dol = soup_function()
-    
-    a = str(propriete).find('Gas')
-    b = str(propriete).find('Gas+')
-    c = str(propriete).find('Gazole')
-    d = str(propriete).find('Gazole+')
+    finding1 = str(propriete).find('Gas')
+    finding2 = str(propriete).find('Gas+')
+    finding3 = str(propriete).find('Gazole')
+    finding4 = str(propriete).find('Gazole+')
 
 
     gas = False
-    gasplus = False 
-    
-    if a or c >= 0: 
+    gasplus = False
+
+    if finding1 or finding3 >= 0:
         gas = True
-    elif b or d >=0:
+    elif finding2 or finding4 >= 0:
         gasplus = True
 
     return gas, gasplus, dol
 
 
 
+def function_recup_tag1(gas, gasplus, dol):
+    """Pep8 function"""
+
+    out = ''
+    if gas is True:
+        if gasplus is True:
+            if dol == 'dollars baisse  ':
+                out = 'tres fort'
+
+    if gas is True:
+        if gasplus is False:
+            if dol == 'dollars augmente':
+                out = 'moyen'
+
+    if gas is False:
+        if gasplus is True:
+            if dol == 'dollars baisse  ':
+                out = 'fort'
+
+    return out
+
+def function_recup_tag(gas, gasplus, dol):
+    """Pep8 function"""
+
+    out = ''
+    if gas is False:
+        if gasplus is True:
+            if dol == 'dollars augmente':
+                out = 'moyen'
+
+    if gas is False:
+        if gasplus is False:
+            if dol == 'dollars augmente':
+                out = 'bas'
+
+    if gas is True:
+        if gasplus is False:
+            if dol == 'dollars baisse  ':
+                out = 'fort'
+
+    if out == '':
+        out = function_recup_tag1(gas, gasplus, dol)
+
+    return out
 
 def recup_tag():
     """Here we get diesel and dollars courses
     to analyze in France the sale of siesel and
     therefore the rate of diesel car right now"""
 
-    #finding_function()
     gas, gasplus, dol = finding_function()
-                                                           
-    if gas == True and\
-       gasplus == True and\
-       dol  == 'dollars baisse  ':
-        return 'tres fort'
+    out = function_recup_tag(gas, gasplus, dol)
 
-    elif gas == True and\
-         gasplus == False and\
-         dol == 'dollars augmente':
-        return 'moyen'
-
-    elif gas == False and\
-         gasplus == True and\
-         dol == 'dollars augmente':
-        return 'moyen'
-
-    elif gas == False and\
-         gasplus == False and\
-         dol == 'dollars augmente':
-        return 'bas'
-
-
-    elif gas == True and\
-         gasplus == False and\
-         dol == 'dollars baisse  ':
-        return 'fort'
-
-    elif gas == False and\
-         gasplus == True and\
-         dol == 'dollars baisse  ':
-        return 'fort'
+    return out
