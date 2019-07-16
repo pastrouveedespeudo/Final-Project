bdfd99e8e591bb0e6301780abf716fc12ca2e690
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-17 00:00:37 +0200

    Update day_night.py

--- bobo/polution/data_site/day_night.py
+++ bobo/polution/data_site/day_night.py
@@ -5,28 +5,25 @@ import datetime
 def night_day():
     """Here we just recup information about the moment of
     the day"""
-    
+
     night = [22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8]
 
     date = datetime.datetime.now()
     hour = date.hour
-    
-    if hour == 23: 
+
+    if hour == 23:
         hour = 1
     elif hour == 24:
         hour = 2
     else:
-        hour + 2 
+        hour = hour + 2
 
-    the_night = ''
-    no_night = ''
+    out = ''
 
-    
     for i in night:
         if i == hour:
-            the_night = 'nuit'
-            return the_night 
+            out = 'nuit'
         else:
-            no_night = 'jour'
+            out = 'jour'
 
-    return no_night
+    return out
