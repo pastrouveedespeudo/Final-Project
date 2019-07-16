36913aec1839ff05e4e04b0b0bf0f07059bcc45e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 23:36:06 +0200

    Update HAIR_CONFIG.py

--- bobo/photo/magasins/HAIR_CONFIG.py
+++ bobo/photo/magasins/HAIR_CONFIG.py
@@ -1,10 +1,19 @@
-LISTE1 = ['coiffure', 'COIFFURE', 'salon', 'salon de coiffure',
-         'Franck Provost', 'coiffeur', 'hairdresser', 'prestige', 'Prestige',
-          'hair', 'bambo', 'haircut', 'Hair', 'Hairdresser', 'Coiffure',
+"""This is config file for Hairdresser magazin folder"""
+
+LISTE1 = ['coiffure', 'COIFFURE', 'salon',
+          'salon de coiffure',
+          'Franck Provost', 'coiffeur',
+          'hairdresser', 'prestige', 'Prestige',
+          'hair', 'bambo', 'haircut',
+          'Hair', 'Hairdresser', 'Coiffure',
           'Coiffeur', 'Hairdressers', 'coiffeuse']
 
-PATH_HAIRDRESSER = 'https://www.google.com/search?q={}+coiffeur&oq={}+coiffeur'
+PATH_HAIRDRESSER = 'https://www.google.com/search?q={}+coiff' +\
+                   'eur&oq={}+coiffeur'
 
-PATH_SCHEDULE = 'https://www.google.com/search?ei=Gnn5XJP5KIu5gweW1qaQBQ&q={0}+{1}+horraires&oq={0}+{1}+horraires'
+PATH_SCHEDULE = 'https://www.google.com/search?ei=Gnn5X' +\
+                'JP5KIu5gweW1qaQBQ&q={0}+{1}+horraires&oq={0}+{1}+horraires'
 
-AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
+AGENT = "Mozilla/5.0 (Macintosh; Intel Ma" +\
+        "c OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chr" +\
+        "ome/50.0.2661.94 Safari/537.36"
