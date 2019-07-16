77e8d2416d279126f84709b26967b714f9ab19a4
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 23:16:31 +0200

    Update graphic_wind.py
    coupe/pollution/un autre truk/pollution2

--- bobo/polution/graph/graphic_wind.py
+++ bobo/polution/graph/graphic_wind.py
@@ -2,13 +2,12 @@
 we recuperate all data from one condition
 and create a matplolib graph"""
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pylab
-import psycopg2
-import numpy as np
-import os
+
 import shutil
+import matplotlib.pyplot as plt
+import psycopg2
+
+
 from .function_graph import moyenne
 from .function_graph import new
 
@@ -17,20 +16,21 @@ from .CONFIG import HOST
 from .CONFIG import USER
 from .CONFIG import PASSWORD
 
+
+
 def visu_wind(city):
     """Here we call database for take wind"""
-    
+
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cursor = conn.cursor()
-    
+
     sql = ("""SELECT vent, nombre_particule FROM vent
             WHERE nom_ville = %s;""")
-    
-    values = (city)
+
 
     cursor.execute(sql, (city,))
 
@@ -40,23 +40,25 @@ def visu_wind(city):
     return liste
 
 
+
 def treatment_wind(data_wind):
     """We split it into list who corresponding to data"""
-    
+
     very_strong = []
     strong = []
     means = []
     low = []
 
- 
+
     for i in data_wind:
         print(i)
-        if i[1] == 'None' or i[1] == None\
-            or i[0] == 'None' or i[0] == None:
-                pass
+        if i[1] in ('None', None)\
+            or i[0] in ('None', None):
+            pass
+
         elif i[0] == 'tres fort':
             very_strong.append(int(i[1]))
-        
+
         elif i[0] == 'fort':
             strong.append(int(i[1]))
 
@@ -69,13 +71,13 @@ def treatment_wind(data_wind):
     #We make an average
     data = len(very_strong) + len(strong) + len(means) + len(low)
     print(data)
-    
+
     data_very_strong = moyenne(very_strong)
     data_strong = moyenne(strong)
     data_means = moyenne(means)
     data_low = moyenne(low)
 
-    
+
     return data_very_strong[0], data_strong[0],\
            data_means[0], data_low[0],\
            data_very_strong[1], data_strong[1],\
@@ -83,57 +85,30 @@ def treatment_wind(data_wind):
 
 
 def diagram_wind(data_very_strong, data_strong, data_means, data_low,
-              er_data_very_strong, er_data_strong,
-                 er_data_means, er_data_low, save):
+                 er_data_very_strong, er_data_strong,
+                 er_data_means, er_data_low,):
 
     """We create a graph and return it"""
     plt.bar(range(4), [data_very_strong, data_strong,
                        data_means, data_low],
-                        width = 0.1, color = 'black',
-                       yerr = [er_data_very_strong, er_data_strong,
-                               er_data_means, er_data_low],
-                        ecolor = 'black', capsize = 10)
-                
+            width=0.1, color='black',
+            yerr=[er_data_very_strong, er_data_strong,
+                  er_data_means, er_data_low],
+            ecolor='black', capsize=10)
 
 
-    plt.xticks(range(4), ['vent tres fort', 'vent fort', 'vent moyen','vent faible'])
+    plt.xticks(range(4), ['vent tres fort', 'vent fort',
+                          'vent moyen', 'vent faible'])
 
-        
+
     plt.ylabel('Taux de pollution en AQI')
     plt.title("Taux de pollution selon le vent en km/h")
-    
+
     nouveau = new()
-    
+
     plt.savefig(nouveau, transparent=True)
     plt.clf()
     plt.close()
-    
+
     shutil.move(nouveau, '/app/static/popo')
     return nouveau
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
