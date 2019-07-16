ff9b897a8f34f3e82fcbcdd8ca24ade3067ce022
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 23:02:24 +0200

    Update graphic_season.py
    j'y peut rien les arguments gros pfffff

--- bobo/polution/graph/graphic_season.py
+++ bobo/polution/graph/graphic_season.py
@@ -2,13 +2,9 @@
 we recuperate all data from one condition
 and create a matplolib graph"""
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pylab
-import psycopg2
-import numpy as np
-import os
 import shutil
+import matplotlib.pyplot as plt
+import psycopg2
 
 from .function_graph import moyenne
 from .function_graph import new
@@ -18,22 +14,22 @@ from .CONFIG import HOST
 from .CONFIG import USER
 from .CONFIG import PASSWORD
 
+
+
 def visu_season(city):
     """Here we call database for take season"""
-    
+
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cursor = conn.cursor()
-    
+
     sql = ("""SELECT saison, nombre_particule FROM saison
             WHERE nom_ville = %s;""")
-    
-    values = (city)
 
-    cursor.execute(sql, (city,))
+    cursor.execute(sql, (city, ))
 
     rows = cursor.fetchall()
     liste = [i for i in rows]
@@ -52,22 +48,26 @@ def treatment_season(data_season):
 
     for i in data_season:
         print(i)
-        if i[1] == 'None' or i[1] == None\
-            or i[0] == 'None' or i[0] == None:
-                pass
+        if i[1] in ('None', None)or\
+            i[0] in ('None', None):
+            pass
+
         elif i[0] == 'primtemps':
             spring.append(int(i[1]))
+
         elif i[0] == 'été':
             summer.append(int(i[1]))
-        elif i[0] == 'hiver':
-            winter.append(int(i[1]))  
-        elif i[0] == 'automne':
-            autumn.append(int(i[1])) 
 
-    #We make an average
+        elif i[0] == 'hiver':
+            winter.append(int(i[1]))
+
+        elif i[0] == 'automne':
+            autumn.append(int(i[1]))
+
+
     data = len(spring) + len(summer) + len(winter) + len(autumn)
     print(data)
-    
+
     data_spring = moyenne(spring)
     data_summer = moyenne(summer)
     data_winter = moyenne(winter)
@@ -81,47 +81,28 @@ def treatment_season(data_season):
 
 
 def diagram_season(data_spring, data_summer, data_winter, data_autumn,
-              er_sping, er_summer, er_winter, er_autumn, save):
-
+                   er_sping, er_summer, er_winter, er_autumn):
     """We create a graph and return it"""
+
     plt.bar(range(4), [data_spring, data_summer, data_winter,
                        data_autumn],
-                        width = 0.1, color = 'black',
-                       yerr = [er_sping, er_summer, er_winter,
-                              er_autumn],
-                        ecolor = 'black', capsize = 10)
-                
+            width=0.1, color='black',
+            yerr=[er_sping, er_summer, er_winter,
+                  er_autumn],
+            ecolor='black', capsize=10)
 
 
     plt.xticks(range(4), ['primtemps', 'été', 'hiver',
                           'automne'])
 
-        
+
     plt.ylabel('Taux de pollution en AQI')
     plt.title("Taux de pollution selon les saisons")
-
     nouveau = new()
-    
+
     plt.savefig(nouveau, transparent=True)
     plt.clf()
     plt.close()
-    
+
     shutil.move(nouveau, '/app/static/popo')
     return nouveau
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
