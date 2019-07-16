a656603aecb74d169103397c1934276e5edeb0ab
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-17 00:36:57 +0200

    Update fertilizer.py

--- bobo/polution/data_site/fertilizer.py
+++ bobo/polution/data_site/fertilizer.py
@@ -5,13 +5,11 @@ import datetime
 
 def period_fertilizer():
     """Fertilizer period is between May and June"""
-    month_fertilizer = [5,6]
+    month_fertilizer = [5, 6]
 
     date = datetime.datetime.now()
-    
-    day = date.day
+
     month = date.month
-    year = date.year
 
     for i in month_fertilizer:
         if month == i:
