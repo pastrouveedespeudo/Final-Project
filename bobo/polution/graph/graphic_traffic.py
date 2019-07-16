5724d2db86b38e8c99c1c3392afd35d8b058df5d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 23:05:16 +0200

    Update graphic_traffic.py

--- bobo/polution/graph/graphic_traffic.py
+++ bobo/polution/graph/graphic_traffic.py
@@ -2,13 +2,11 @@
 we recuperate all data from one condition
 and create a matplolib graph"""
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pylab
-import psycopg2
-import numpy as np
-import os
+
 import shutil
+import matplotlib.pyplot as plt
+import psycopg2
+
 from .function_graph import moyenne
 from .function_graph import new
 
@@ -20,20 +18,19 @@ from .CONFIG import PASSWORD
 
 def visu_traffic(city):
     """Here we call database for take traffic"""
-    
+
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cursor = conn.cursor()
-    
+
     sql = ("""SELECT TRAFIQUE, nombre_particule FROM traffique
             WHERE nom_ville = %s;""")
-    
-    values = (city)
 
-    cursor.execute(sql, (city,))
+
+    cursor.execute(sql, (city, ))
 
     rows = cursor.fetchall()
     liste = [i for i in rows]
@@ -43,112 +40,60 @@ def visu_traffic(city):
 
 def treatment_traffic(data_traffic):
     """We split it into list who corresponding to data"""
-    
+
     deaparture = []
     regular_day = []
 
- 
     for i in data_traffic:
-    
-        if i[0] == None or i[0] == 'None' or\
-           i[1] == None or i[1] == 'None':
+
+        if i[0] in (None, 'None') or\
+           i[1] in (None, 'None'):
             pass
+
         elif i[0] == 'regulier jour':
             regular_day.append(int(i[1]))
+
         elif i[0] == 'depart_routier':
             deaparture.append(int(i[1]))
 
         print(i)
-        
+
     data = len(deaparture) + len(regular_day)
     print(data)
 
-    #We make an average
+
     data_regular_day = moyenne(regular_day)
     data_deaparture = moyenne(deaparture)
+
 
     return data_regular_day[0], data_deaparture[0],\
             data_regular_day[1], data_deaparture[1], data
 
+
+
 def diagram_traffic(data_regular_day, data_deaparture,
-              er_regular_day, er_deaparture, save):
+                    er_regular_day, er_deaparture):
 
     """We create a graph and return it"""
-    
+
     plt.bar(range(2), [data_regular_day, data_deaparture],
-                        width = 0.1, color = 'black',
-                       yerr = [er_regular_day, er_deaparture],
-                        ecolor = 'black', capsize = 10)
-                
+            width=0.1, color='black',
+            yerr=[er_regular_day, er_deaparture],
+            ecolor='black', capsize=10)
+
 
 
     plt.xticks(range(2), ['régulier jour', 'départ routier'])
 
-        
+
     plt.ylabel('Taux de pollution en AQI')
     plt.title("Taux de pollution selon les départ routiers")
 
     nouveau = new()
-    
+
     plt.savefig(nouveau, transparent=True)
     plt.clf()
     plt.close()
-    
+
     shutil.move(nouveau, '/app/static/popo')
     return nouveau
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
