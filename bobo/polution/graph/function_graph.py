767cf63bf27fd4be915d609d468073ba7292027f
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 20:01:40 +0200

    Update function_graph.py

--- bobo/polution/graph/function_graph.py
+++ bobo/polution/graph/function_graph.py
@@ -1,69 +1,51 @@
-import numpy as np
-import os
+"""Here we give the name to the new graphe,
+    we check in popo folder take the last graph
+    and add +1."""
 
+import os
+import numpy as np
 
 def moyenne(liste):
+    """Here we define the mean and the error"""
+
     try:
-        moyenne = int(sum(liste)) / int(len(liste))
-    except:
-        moyenne = 0
+        the_mean = int(sum(liste)) / int(len(liste))
+    except ZeroDivisionError:
+        the_mean = 0
     variance = np.var(liste)
 
     erreur = (variance/len(liste))**(1/2)
-    return moyenne, erreur
+    return the_mean, erreur
 
 
 
 def new():
+    """Here we define the new graphic"""
 
     liste_new = []
     liste = os.listdir('/app/static/popo')
-    #liste = os.listdir(r'C:\Users\jeanbaptiste\bobo\bobo\static\popo')
+
     for i in liste:
-        
         try:
-            nb = str((i[-7])) + str((i[-6])) + str((i[-5]))
-            nb = int(nb)
-            liste_new.append(nb)
-        except:
+            number = str((i[-7])) + str((i[-6])) + str((i[-5]))
+            number = int(number)
+            liste_new.append(number)
+        except IndexError:
             pass
-        
+
         try:
-            nb = str((i[-6])) + str((i[-5]))
-            nb = int(nb)
-            liste_new.append(nb)
-        except:
+            number = str((i[-6])) + str((i[-5]))
+            number = int(number)
+            liste_new.append(number)
+        except IndexError:
             try:
-                nb = int(i[-5])
-                liste_new.append(nb)
-            except:
+                number = int(i[-5])
+                liste_new.append(number)
+            except IndexError:
                 pass
 
     maxi = max(liste_new) + 1
 
-
-        
-    new_save = str(liste[-1][:-5]) + str(maxi) + '.png' 
+    new_save = str(liste[-1][:-5]) + str(maxi) + '.png'
 
     return new_save
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
