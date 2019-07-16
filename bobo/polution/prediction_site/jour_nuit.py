56b3fd8e7977890f1e4b078d036cf5405517db83
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-16 17:12:38 +0200

    Update jour_nuit.py

--- bobo/polution/prediction_site/jour_nuit.py
+++ bobo/polution/prediction_site/jour_nuit.py
@@ -5,7 +5,7 @@ import datetime
 
 def nuit_jour():
     """Define day or night period"""
-    
+
     nuit = [22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8]
 
 
@@ -16,18 +16,14 @@ def nuit_jour():
     elif heure == 24:
         heure = 2
     else:
-         heure = heure + 2
-    #heure + 2
+        heure = heure + 2
 
-    la_nuit = ''
-    non_nuit = ''
-
-    
+    the_periode = ''
     for i in nuit:
         if i == heure:
-            la_nuit = 'nuit'
-            return la_nuit
-        else:
-            non_nuit = 'jour'
+            the_periode = 'nuit'
 
-    return non_nuit
+        else:
+            the_periode = 'jour'
+
+    return the_periode
