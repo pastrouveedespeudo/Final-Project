ba464567e6b7498973e5cec87f23253805ef8ae7
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-17 14:42:02 +0200

    Update creation_database.py

--- bobo/photo/data_tchat/creation_database.py
+++ bobo/photo/data_tchat/creation_database.py
@@ -15,16 +15,16 @@ def create_database_coupe():
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
+
     cur.execute("""CREATE TABLE tchat_coupe(
                 id serial PRIMARY KEY,
                 message TEXT,
                 date varchar(20))
                 """)
-    
+
     conn.commit()
 
 def create_database_habit():
@@ -33,16 +33,16 @@ def create_database_habit():
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
+
     cur.execute("""CREATE TABLE tchat_habit(
                 id serial PRIMARY KEY,
                 message TEXT,
                 date varchar(20))
                 """)
-    
+
     conn.commit()
 
 
@@ -52,16 +52,16 @@ def create_database_tendance():
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
+
     cur.execute("""CREATE TABLE tchat_tendance(
                 id serial PRIMARY KEY,
                 message TEXT,
                 date varchar(20))
                 """)
-    
+
     conn.commit()
 
 
@@ -70,4 +70,3 @@ if __name__ == "__main__":
     create_database_coupe()
     create_database_habit()
     create_database_tendance()
-
