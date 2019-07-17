2a4573de23cfffd3081819cb8bc594a195c926aa
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 00:34:30 +0200

    Update fertilizer.py

--- bobo/polution/data_site/fertilizer.py
+++ bobo/polution/data_site/fertilizer.py
@@ -1,10 +1,18 @@
-"""We return fetilizer period"""
+"""We recup fertilizer data for increment database.
+The fertilizer period is in June and May.
+We define the current period and matching it to mois_angrais.
+If this matching so we return yes else we return None."""
 
 
 import datetime
 
 def period_fertilizer():
-    """Fertilizer period is between May and June"""
+    """We run the loop and put conditions.
+    If there is a comparaison so we return
+    yes and break the loop. Else
+    (no comparaison) we define out like None."""
+
+
     month_fertilizer = [5, 6]
 
     date = datetime.datetime.now()
