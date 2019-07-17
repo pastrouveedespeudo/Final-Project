b2d556cac3a41a01e4f75d838b2e30dd6a22ab4f
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-17 21:36:06 +0200

    Update views.py

--- bobo/direction/views.py
+++ bobo/direction/views.py
@@ -14,12 +14,19 @@ from .data_tchat.database import tchat
 
 
 def navebarre_vent(request):
-    """This menu of wind function"""
+    """we call it navebarre but it's a menu.
+    We make appear a new page who's a menu from map template."""
+
     return render(request, "menu/navebarre_vent.html")
 
 
 def map1(request):
-    """This is template for tchat map"""
+    """This is template for tchat map. We call
+    views function if there is a request type post,
+    which serves as a relief to the views.
+    We make sure there is no big word,
+    spam or empty message to put the message in database.
+    If we just call the template, we return all message from database."""
 
     if request.method == "POST":
 
@@ -29,12 +36,16 @@ def map1(request):
 
     message = tchat()
 
-
     return render(request, "map1.html", {"message":message})
+
+
 
 @ensure_csrf_cookie
 def map(request):
-    """This is map template"""
+    """This is map template. There are 2
+    call, data and data2. Data is for the fisrt call ex Lyon
+    from France. Next is data2 where we recup a data from template.
+    We must trait it on a new function !"""
 
     if request.method == "POST":
 
@@ -43,11 +54,9 @@ def map(request):
 
         if data:
             nouvelle_position, lat, long = function_map(data)
-
             return HttpResponse((lat, ' ', long, ' ', nouvelle_position))
 
         if data2:
-
             lat, long, listee, index = function_map2(data2)
             nouvelle_position, lat, long = function_map3(lat,
                                                          long,
@@ -62,6 +71,7 @@ def map(request):
 
 
 
-if __name__ == "__main__":
-
+if __name__ == "__main__":#This is strang but... Main from .carte
+                          #need to call directly for execute the code.
+    
     map(request)
