bb21a001a696cc418f791b620021dec7def8ff54
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-17 14:45:05 +0200

    Update database.py

--- bobo/photo/data_tchat/database.py
+++ bobo/photo/data_tchat/database.py
@@ -1,13 +1,13 @@
 """We insert into database or call it for return data"""
 
+
+import datetime
 import psycopg2
 
 from .CONFIG import HOST
 from .CONFIG import USER
 from .CONFIG import PASSWORD
 from .CONFIG import DATABASE
-
-import datetime
 
 
 
@@ -21,24 +21,24 @@ def database_coupe(data):
 
     date = day, month, year
     date = str(date)
-    
+
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
+
     cur.execute("""INSERT INTO tchat_coupe
                 (message, date)
                 values(%s, %s)
                 """, (data, date))
-    
+
     conn.commit()
 
 def database_habit(data):
     """Insert into databse"""
-    
+
     date = datetime.datetime.now()
     day = date.day
     month = date.month
@@ -46,25 +46,25 @@ def database_habit(data):
 
     date = day, month, year
     date = str(date)
-    
+
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
+
     cur.execute("""INSERT INTO tchat_habit
                 (message, date)
                 values(%s, %s)
                 """, (data, date))
-    
+
     conn.commit()
 
 
 def database_tendance(data):
     """Insert into databse"""
-    
+
     date = datetime.datetime.now()
     day = date.day
     month = date.month
@@ -72,23 +72,20 @@ def database_tendance(data):
 
     date = day, month, year
     date = str(date)
-    
+
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
+
     cur.execute("""INSERT INTO tchat_tendance
                 (message, date)
                 values(%s, %s)
                 """, (data, date))
-    
+
     conn.commit()
-
-
-
 
 
 
@@ -99,22 +96,19 @@ def tchat_coupe():
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
-    cur.execute("""
-                select * from tchat_coupe;
-                """)
 
-    
+    cur.execute("""select * from tchat_coupe;""")
+
     conn.commit()
-    
+
     rows = cur.fetchall()
     liste = [i for i in rows]
-    
+
     liste1 = traitement(liste)
-    
+
     return liste1
 
 
@@ -125,22 +119,18 @@ def tchat_habit():
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
-    cur.execute("""
-                select * from tchat_habit;
-                """)
 
-    
+    cur.execute("""select * from tchat_habit;""")
+
     conn.commit()
-    
+
     rows = cur.fetchall()
     liste = [i for i in rows]
-    
     liste1 = traitement(liste)
-    
+
     return liste1
 
 
@@ -150,35 +140,25 @@ def tchat_tendance():
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
                             host=HOST,
-                            password=PASSWORD) 
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
-    cur.execute("""
-                select * from tchat_tendance;
-                """)
 
-    
+    cur.execute("""select * from tchat_tendance;""")
+
     conn.commit()
-    
     rows = cur.fetchall()
     liste = [i for i in rows]
-    
     liste1 = traitement(liste)
-    
+
     return liste1
-
-
-
-
 
 
 
 def traitement(liste):
     """We insert data into liste"""
+
     liste1 = []
     for i in liste:
         liste1.append([i[1], i[2]])
-
-
     return liste1
