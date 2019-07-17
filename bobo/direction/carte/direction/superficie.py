cd00f562ddcc6dc3c62dc5bbc8c7d8c79855a56d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 00:02:26 +0200

    Update superficie.py

--- bobo/direction/carte/direction/superficie.py
+++ bobo/direction/carte/direction/superficie.py
@@ -7,7 +7,12 @@ from .CONFIG import PATH_SUPERFICIE
 
 
 def function_superficie_ville(ville):
-    """We calling the page with bs4"""
+    """Here we define the url including the city.
+    All urls have the same schemas.
+    You just have to enter the city.
+    Then we ask for bs4 then we look
+    for the adapted tag here we take
+    all the div we put in a list"""
 
     path = PATH_SUPERFICIE.format(ville, ville)
     request_html = requests.get(path)
@@ -19,7 +24,12 @@ def function_superficie_ville(ville):
 
 
 def function_superficie_ville1(propriete):
-    """Here we seach km² and recup the int before it"""
+    """We search among all the div of the numbers
+    (if there are digits there is a comma we replace it by a point),
+    if at the end of this number there is a: '²'
+    then we stop the loop and we recover
+    the number that the 'we return"""
+
 
     liste = []
 
@@ -36,18 +46,25 @@ def function_superficie_ville1(propriete):
             counter1 = 0
             if j in (',', '.'):
                 number += str('.')
+                #Transform , to . ex: 25,55 = 25.55
             try:
                 j = int(j)
+                #transform element to int(element)
                 if j == int(j):
                     number += str(j)
+                    #if number is an int, we increment number
             except ValueError:
                 pass
+
             if j == '²' and number != '':
                 kilometre_carre = True
                 break
+                #if element == '²' we break and kilometre_carre = True
             counter1 += 1
         if kilometre_carre is True:
             break
+        #If kilometre_carre is True so we break loop
+        #We have our superficy !
     counter += 1
 
 
@@ -55,8 +72,11 @@ def function_superficie_ville1(propriete):
 
 
 def superficie_ville(ville):
-    """Now we transforme the , to . and
-    make it float"""
+    """Here we make sure we have a right data.
+    If we have wrong, we increment the final variable to 20.00
+    a little city is 20kms.
+    We verify the points because we sometimes have
+    for example data like ..........20.26"""
 
     propriete = function_superficie_ville(ville)
     number = function_superficie_ville1(propriete)
