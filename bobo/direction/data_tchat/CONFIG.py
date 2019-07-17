9ba9de47bb9de532bae2f4d67a6525f9e9894c8d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-17 14:36:16 +0200

    Update CONFIG.py

--- bobo/direction/data_tchat/CONFIG.py
+++ bobo/direction/data_tchat/CONFIG.py
@@ -1,3 +1,5 @@
+"""This is variable for database"""
+
 HOST = "ec2-46-137-91-216.eu-west-1.compute.amazonaws.com"
 USER = "vobmeocqilozfo"
 PASSWORD = "81886977eb54552e57161a1afcb236fb4a21ad3681abbad6921d72f6c41322c6"
