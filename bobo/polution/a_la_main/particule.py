2ab24c8e606819cd29377b83948bd7d24dfaad97
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-17 17:23:26 +0200

    Update particule.py

--- bobo/polution/a_la_main/particule.py
+++ bobo/polution/a_la_main/particule.py
@@ -1,28 +1,25 @@
-"""Here we define for database
+"""Here we define for site web
 the numbers of particle,
 ranking pollute in France and industrail poles"""
 
 import requests
-import datetime
-import urllib.request
-from bs4 import *
-
+from bs4 import BeautifulSoup
 
 from CONFIG import PATH_PARTICLE_RATE
 
 def particule2(lieu):
     """we search particule rate from plumelabs"""
 
-    nb = []
+    numbers = []
     liste = []
 
     path = PATH_PARTICLE_RATE.format(lieu)
     request = requests.get(path)
     page = request.content
     soup_html = BeautifulSoup(page, "html.parser")
-    Property = soup_html.find_all("div", {'class':'report__pi-number'})
+    properties = soup_html.find_all("div", {'class':'report__pi-number'})
 
-    for i in Property:
+    for i in properties:
         liste.append(i.get_text())
 
     for i in liste:
@@ -30,59 +27,46 @@ def particule2(lieu):
             try:
                 j = int(j)
                 if j == int(j):
-                    nb.append(str(j))
-            except:
+                    numbers.append(str(j))
+            except ValueError:
                 pass
 
-            
-    nb = ''.join(nb)
-    nb = int(nb)
-    polution = nb
+    numbers = ''.join(numbers)
+    numbers = int(numbers)
 
-    return polution
+    return numbers
 
 
-    
-def france(lieu):
+def france(place):
+    """France ranking"""
+
+    out = ''
+
+    if place == 'lyon':
+        out = 'un'
+    elif place == 'marseille':
+        out = 'deux'
+
+    elif place == 'paris':
+        out = 'trois'
+
+    elif place == "roubaix":
+        out = 'quattre'
+    else:
+        out = 'non'
+
+    return out
 
 
-    liste = ["lyon", "marseille","paris","roubaix"]
 
+def industrie(place):
+    """Industrie cities"""
 
-    c = 0
-    for i in liste:
-
-        if lieu == liste[0]:
-            return 'un'
-            break
-
-        elif lieu == liste[1]:
-            return 'deux'
-            break
-
-        elif lieu == liste[2]:
-            return 'trois'
-            break
-
-        elif lieu == liste[3]:
-            return 'quattre'
-            break
-        else:
-            return 'non'
-            break
-            
-
-            
-        c+=1
-    
-
-
-def industrie(lieu):
-
-
-    if lieu == 'lyon':
-        return 'oui'
-    elif lieu == 'paris':
-        return 'non'
-    elif lieu == 'marseille':
-        return 'oui'
+    out = ''
+    if place == 'lyon':
+        out = 'oui'
+    elif place == 'paris':
+        out = 'non'
+    elif place == 'marseille':
+        out = 'oui'
+    return out
