18df9aaf0fe66769e00e702a83958bc29e0e46c5
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 00:19:10 +0200

    Update gym.py

--- bobo/photo/magasins/gym.py
+++ bobo/photo/magasins/gym.py
@@ -1,4 +1,8 @@
-"""This is functions for search gym area from bs4"""
+"""This is functions for search gym area from bs4
+We search by scrapping all data who begening by hair
+or hairdressers for exemple."""
+
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,8 +11,12 @@ from .GYM_CONFIG import PATH_CITY
 from .GYM_CONFIG import AGENT
 from .GYM_CONFIG import PATH_SCHEDULE
 
+
+
 def big_city_gym(city):
-    """Here we search gym into the cities"""
+    """Here we search all span
+    of the site web.
+    And scrap all the span by the GYM variable."""
 
     path = PATH_CITY.format(city)
     request_html = requests.get(path)
@@ -20,39 +28,42 @@ def big_city_gym(city):
     liste1 = []
 
     for i in propriete:
+        #All span on the current page.
+
         for j in GYM:
             var_find = str(i.string).find(str(j))
+            #Cf Config for see GYM variable.
+
             if var_find >= 0:
                 liste1.append(i.string)
+                #If we found element of GYM on the current tag,
+                #we append the string of that (<span>i.string</span>)
+
 
     return liste1
 
 
+
 def schedule_gym(name, city):
-    """We search all schedule from gym found"""
+    """We search on google all elements taken by big_city_gym.
+    And we search all days of a week. If we found it,
+    we add it to a list."""
 
     path = PATH_SCHEDULE.format(name, city)
 
     request_html = requests.get(path, headers={"User-Agent": AGENT})
-
     page = request_html.content
     soup = BeautifulSoup(page, "html.parser", from_encoding="utf-8")
 
-    #propriete = soup.find_all("span")
     propriete = soup.find_all("td")
 
     liste = []
     for i in propriete:
         liste.append(i.string)
 
-
-##    week = ['lundi', 'mardi', 'mercredi', 'jeudi',
-##            'vendredi', 'samedi', 'dimanche',]
-
-
     week = ['Monday', 'Tuesday', 'Wednesday',
             'Thursday', 'Friday', 'Saturday', 'Sunday',]
-
+    #All days of the week. We searching it on google.
 
     liste1 = []
     count = 0
@@ -61,10 +72,13 @@ def schedule_gym(name, city):
 
         for j in week:
             var_find = str(i).find(str(j))
+            #We search all element of week variable.
+
             if var_find >= 0:
                 liste1.append([i, liste[count + 1]])
+                #If we find it we add it to liste1.
+                #The count allows us to do a loop tour of two lists
 
         count += 1
-
 
     return liste1
