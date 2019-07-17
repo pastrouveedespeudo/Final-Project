d10ceadf53f391bdd795bf268a2741e6820cd7f6
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 00:35:34 +0200

    Update angrais.py
    aaaaah un truk sans . pour a la main et prediction et data site c pas les meme variable !! ouf

--- bobo/polution/prediction_site/angrais.py
+++ bobo/polution/prediction_site/angrais.py
@@ -1,9 +1,15 @@
-"""We recup fertilizer data for site web"""
+"""We recup fertilizer data for increment database.
+The fertilizer period is in June and May.
+We define the current period and matching it to mois_angrais.
+If this matching so we return yes else we return None."""
 
 import datetime
 
 def periode_angrais():
-    """We define the month and match it with fertilizer period"""
+    """We run the loop and put conditions.
+    If there is a comparaison so we return
+    yes and break the loop. Else
+    (no comparaison) we define out like None."""
 
     mois_angrais = [5, 6]
 
