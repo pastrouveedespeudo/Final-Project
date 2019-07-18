bc4eb576f67289a87a8f9e515185d324bc67793b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 14:15:22 +0200

    Update tendance.py

--- bobo/photo/tendance_file/analysis_database/tendance.py
+++ bobo/photo/tendance_file/analysis_database/tendance.py
@@ -1,4 +1,4 @@
-
+"""Here we define the main colors of picture"""
 
 import psycopg2
 
@@ -41,13 +41,13 @@ def i_into_i(liste):
     the 2nd element is ',' ... so you insert to position counter
     the current row."""
 
-    LISTE1 = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],]
+    the_liste1 = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],]
 
     counter = 0
     counter1 = 0
@@ -65,18 +65,18 @@ def i_into_i(liste):
             pass
 
         else:
-            LISTE1[counter].append(i)
+            the_liste1[counter].append(i)
 
         counter1 += 1
 
-    return LISTE1
+    return the_liste1
 
 
 
 def unification(liste1):
     """So we get this :['"', 'b', 'l', 'e', 'u'....
     we must join it now."""
-    
+
     liste2 = []
     for i in liste1:
         if i == []:
@@ -84,7 +84,7 @@ def unification(liste1):
         else:
             i = "".join(i)
             liste2.append(i)
-            
+
     return liste2
 
 
@@ -94,32 +94,28 @@ def suppression_en_trop(liste2):
     For do that we ignore all -> ) " , ( ][
     and add to mot variable the rest.
     At the end of loop we add it to a list."""
-    
+
     liste3 = []
-    
+
     for i in liste2:
         liste3.append(i.split())
 
     liste4 = []
     for i in liste3:
-        
+
         for j in i:
             mot = ''
-            
+
             for k in j:
-                if k == '(' or\
-                   k == '"' or\
-                   k == ',' or\
-                   k == ')' or\
-                   k ==']':
+                if k in ('(', '"', ',', ')', ']'):
                     pass
-                
+
                 else:
                     mot += k
-                    
+
             liste4.append(mot)
 
-    return liste4 
+    return liste4
 
 
 
@@ -129,7 +125,7 @@ def re_elment_de_liste(liste4):
     the first part of the list of list is a list of
     color and the number of color pixel. The second
     is the name + top or down part of the picture.
-    We need to have no 2 part into list of list
+    We need to have now 2 part into list of list
     but a unique list.
     [ ("bleu", 1), ("beige", 8), ("marron", 3),
     ("blanc", 44), ("noir", 2), ("rouge", 239),
@@ -139,41 +135,39 @@ def re_elment_de_liste(liste4):
     'marron', '3', 'blanc', '44',
     'noir', '2', 'rouge', '239',
     'gris', '25', '2a.jpg', 'haut']
- 
     """
 
 
-    LISTE5 = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-              [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],]
+    the_liste5 = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],]
 
 
 
     counter = 0
     for i in liste4:
-        
-        if i == 'bas' or\
-           i == 'haut':
-            
-            LISTE5[counter].append(i)
+
+        if i in ('bas', 'haut'):
+
+            the_liste5[counter].append(i)
             counter += 1
-            
+
         elif i == '':
             pass
-        
+
         else:
-            LISTE5[counter].append(i)
+            the_liste5[counter].append(i)
 
     liste6 = []
-    for i in LISTE5:
-        
+    for i in the_liste5:
+
         if i == []:
             pass
 
@@ -188,25 +182,25 @@ def mise_en_dico(liste6):
     in dictionnary
     For example:
     bleu': '1', 'beige': '8',"""
-    
+
     liste7 = []
     dico = {}
 
     counter = 0
     for i in liste6:
-        
+
         counter1 = 0
-        
+
         for j in i:
-            
+
             try:
                 dico[liste6[counter][counter1]] = liste6[counter][counter1 + 1]
                 counter1 += 2
-                
+
             except:
                 pass
 
-            
+
         liste7.append(dico)
         dico = {}
         counter += 1
@@ -234,7 +228,7 @@ def determination_couleur(liste7):
         couleur = ''
         nombre = 0
         nom = []
-        
+
         for cle, valeur in i.items():
 
             try:
@@ -243,29 +237,29 @@ def determination_couleur(liste7):
                     nombre += int(valeur)
                     couleur = ''
                     couleur = cle
-                    
+
             except:
                 if cle == 'blanc' and\
                    valeur == 0:
-                    
+
                     couleur = 'blanc'
                     nom.append(valeur)
-                    
+
                 elif cle == 'gris' and\
                      valeur == 0:
-                    
+
                     couleur = 'gris'
                     nom.append(valeur)
-                    
+
             if cle[-4:] == '.jpg':
                 nom.append((cle, valeur))
-                
+
             elif valeur[-4:] == '.jpg':
                 nom.append((cle, valeur))
-                
+
 
         liste8.append((couleur, nom))
-        
+
     return liste8
 
 
@@ -277,13 +271,13 @@ def les_tendances_couleurs(liste8):
     ('bleu', '2a.jpg', 'bas', 'marron')
     where marron is the color of haircut."""
 
-    
+
     #We call class visu from coupe_analysis.py
     #It return hair color of picture
     data_coupe = recup2()
-    
 
-    coupa = [(i[1], i[2]) for i in data_coupe] 
+
+    coupa = [(i[1], i[2]) for i in data_coupe]
     coupa2 = [i[1] for i in sorted(coupa)]
 
     counter = 0
@@ -292,23 +286,23 @@ def les_tendances_couleurs(liste8):
 
         if i[0] == '':
             if counter % 2 == 0:
-                ou = 'haut'
+                where = 'haut'
             else:
-                ou = 'bas'
-            liste9.append((i[1][0][0], i[1][0][1], ou))
-            
+                where = 'bas'
+            liste9.append((i[1][0][0], i[1][0][1], where))
+
         else:
             try:
                 if int(i[1][0][0]):
                     if counter % 2 == 0:
-                        ou = 'haut'
+                        where = 'haut'
                     else:
-                        ou = 'bas'
-                    
-                    liste9.append((i[0], i[1][0][1], ou))
+                        where = 'bas'
+
+                    liste9.append((i[0], i[1][0][1], where))
             except:
                 liste9.append((i[0], i[1][0][0], i[1][0][1]))
-            
+
 
         counter += 1
 
@@ -317,7 +311,7 @@ def les_tendances_couleurs(liste8):
         liste9[counter1] = liste9[counter1] + (i,)
         liste9[counter1 + 1] = liste9[counter1 + 1] + (i, )
         counter1 += 2
-        
+
     return liste9
 
 
@@ -331,28 +325,29 @@ def dico_max(dico):
     the number.
     If the value is > number
     we redefine the number and coul who is the key.
-    Like number = 0
-    bleu = 5 so number < value
-    rouge = 10 so number < value
-    green = 1 so we ignore it
+    Like
+    number = 0
+    bleu = 5 so number < value: number = 5
+    rouge = 10 so number < value: number = 10
+    green = 1 so we ignore it: number = 10
     and we return the final key.
     """
 
-    nb = 0
+    number = 0
     coul = ''
-    
+
     for cle, valeur in dico.items():
-        if valeur > nb:
-            
-            nb = 0
-            nb += valeur
+        if valeur > number:
+
+            number = 0
+            number += valeur
             coul = ''
             coul = cle
 
     return coul
 
 
-def analyse_tendance_funct_1(liste, c):
+def analyse_tendance_funct_1(liste, counter):
     """Now we must define all of this
     by haircuts color.
     We ask the list, your 4th element is marron ? blond?...
@@ -362,18 +357,18 @@ def analyse_tendance_funct_1(liste, c):
     liste1 = []
     liste2 = []
     liste3 = []
-    
+
     for i in liste:
         try:
-            if liste[c][3] == 'marron':
-                liste1.append(liste[c][0])
-            elif liste[c][3] == 'blond':
-                liste2.append(liste[c][0])
-            elif liste[c][3] == 'chatin':
-                liste3.append(liste[c][0])
-            
-            c+=2
-            
+            if liste[counter][3] == 'marron':
+                liste1.append(liste[counter][0])
+            elif liste[counter][3] == 'blond':
+                liste2.append(liste[counter][0])
+            elif liste[counter][3] == 'chatin':
+                liste3.append(liste[counter][0])
+
+            counter += 2
+
         except:
             pass
 
@@ -385,10 +380,10 @@ def analyse_tendance_funct_1(liste, c):
 def analyse_tendance_funct_2(liste):
     """We loop a list, and a dictionnary.
     If element of list == key we add + 1 to value dictionnary
-    It will say to us the highter color""""
+    It will say to us the highter color"""
 
     dico = {}
-    
+
     for i in liste:
         dico[i] = 0
 
@@ -399,7 +394,30 @@ def analyse_tendance_funct_2(liste):
 
     return dico
 
+def function_analyse(liste_bas_marron,
+                     liste_bas_blond,
+                     liste_bas_chatain):
+    """Associate to analyse_tendance
+    We define color from picture
+    Like blond == bleu top, red bot for example"""
+    dico_bas_marron = analyse_tendance_funct_2(liste_bas_marron)
+    dico_bas_blond = analyse_tendance_funct_2(liste_bas_blond)
+    dico_bas_chatin = analyse_tendance_funct_2(liste_bas_chatain)
 
+
+    #Take the most value
+    haut_marron = dico_max(dico_haut_marron)
+    haut_blond = dico_max(dico_haut_blond)
+    haut_chatain = dico_max(dico_haut_chatin)
+
+    bas_marron = dico_max(dico_bas_marron)
+    bas_blond = dico_max(dico_bas_blond)
+    bas_chatain = dico_max(dico_bas_chatin)
+
+
+    marron = [haut_marron, bas_marron]
+    blond = [haut_blond, bas_blond]
+    chatain = [haut_chatain, bas_chatain]
 
 def analyse_tendance(liste):
     """We define color from picture
@@ -426,24 +444,10 @@ def analyse_tendance(liste):
     liste_bas_chatain = funt2[2]
 
 
-    dico_bas_marron = analyse_tendance_funct_2(liste_bas_marron)
-    dico_bas_blond = analyse_tendance_funct_2(liste_bas_blond)
-    dico_bas_chatin = analyse_tendance_funct_2(liste_bas_chatain)
-
-
-    #Take the most value
-    haut_marron = dico_max(dico_haut_marron)
-    haut_blond = dico_max(dico_haut_blond)
-    haut_chatain = dico_max(dico_haut_chatin)
-
-    bas_marron = dico_max(dico_bas_marron)
-    bas_blond = dico_max(dico_bas_blond)
-    bas_chatain = dico_max(dico_bas_chatin)
-
- 
-    marron = [haut_marron, bas_marron]
-    blond = [haut_blond, bas_blond]
-    chatain = [haut_chatain, bas_chatain]
+    marron, blond,\
+            chatain = function_analyse(liste_bas_marron,
+                     liste_bas_blond,
+                     liste_bas_chatain)
 
     print(marron, blond, chatain)
     return marron, blond, chatain
@@ -452,7 +456,9 @@ def analyse_tendance(liste):
 
 
 def la_tendance():
-    
+    """We reunicate all of this function
+    and call it from the views"""
+
     liste = dataaa()
     liste1 = i_into_i(liste)
     liste2 = unification(liste1)
@@ -464,18 +470,3 @@ def la_tendance():
     liste10 = analyse_tendance(liste9)
 
     return liste10
-
-
-
-#la_tendance()
-
-
-
-
-
-
-
-
-
-
-
