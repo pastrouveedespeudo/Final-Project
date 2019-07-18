c744eb07f8a96adf078972195877c62275e5e643
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 17:46:59 +0200

    Update addresse.py

--- bobo/direction/carte/test/addresse.py
+++ bobo/direction/carte/test/addresse.py
@@ -1,36 +1,43 @@
-"""Here we search latitude and longititude
-from a city"""
+"""Here we search latitude and longititude from a city"""
 
-import reverse_geocoder as rg 
-import pprint 
-  
-def reverseGeocode(coordinates):
-    """We reverse from have an address
-       and not coordinates"""
-    
-    result = rg.search(coordinates)
-  
+import reverse_geocoder as rervers_g
+
+
+def reverse_geocoderr(coordinates):
+    """Right here we enter a parameter.
+    There are latitude and longitude.
+    It return an address. It's the opposite of normally call.
+    So it call reverse"""
+
+    result = rervers_g.search(coordinates)
     return result
 
 
-
 def dress_to_ville(lat, long):
-    """We reverse from have an address
-       and not coordinates (second technic)"""
-    coordinates =(lat, long) 
-    a = reverseGeocode(coordinates) 
+    """Same as reverse_geocoderr.
+    Here we take the information and recup only
+    a part of the data. Because it return
+    all information on the addresse like 
+    Region, department, dependency ect.
+    So we just take the city and the department.
+    Under out form for the template (who translate it
+    for google map)"""
+
+    coordinates = (lat, long)
+    revers = reverse_geocoderr(coordinates)
+    print(revers)
     liste = []
-    c = 0
-    for i,j in a[0].items():
-        if c == 2:
-            liste.append(j)
-        elif c == 3:
-            liste.append(j)
-        c+=1
+    counter = 0
 
-    ad = str(liste[0]+'+'+liste[1])
+    for _, item_deux in revers[0].items():
+        if counter == 2:
+            liste.append(item_deux)
 
-    return liste[0], ad
+        elif counter == 3:
+            liste.append(item_deux)
 
+        counter += 1
 
+    out = str(liste[0]+'+'+liste[1])
 
+    return liste[0], out
