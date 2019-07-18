2f9c4dee380ab53e1c00d5f08bf3a4db12ce64be
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 17:47:13 +0200

    Update nouvel_pos.py

--- bobo/direction/carte/test/nouvel_pos.py
+++ bobo/direction/carte/test/nouvel_pos.py
@@ -1,7 +1,6 @@
 """Here we calculate the new
-   direction
-   We have calculate
-   a 1 degree = 111.11 km
+   direction. We have calculate
+   a one degree = 111.11 km
    so we *0.009 the number of
    km for have degrees to km and add it to lat
    if direction isn't sud, nord
@@ -12,41 +11,41 @@ from math import cos
 from math import radians
 
 def long_lat_1(sens, lat, long, kilometre):
-    """Function for pep8"""
+    """function to lighten long_lat"""
 
     if sens == 'sudsudouest':
         lat = lat + kilometre
         long = long + kilometre * cos(radians(67.7))
 
-    if sens == 'sudouest':
+    elif sens == 'sudouest':
         lat = lat + kilometre
         long = long + kilometre * cos(radians(45))
 
-    if sens == 'ouestsudouest':
+    elif sens == 'ouestsudouest':
         lat = lat + kilometre
         long = long + kilometre * cos(radians(22.5))
 
-    if sens == 'ouestnordouest':
+    elif sens == 'ouestnordouest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(157.5))
 
-    if sens == 'nordouest':
+    elif sens == 'nordouest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(135))
 
-    if sens == 'nordnordouest':
+    elif sens == 'nordnordouest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(112.5))
 
-    if sens == 'estsudest':
+    elif sens == 'estsudest':
         lat = lat + kilometre
         long = long - kilometre * cos(radians(337))
 
-    if sens == 'sudest':
+    elif sens == 'sudest':
         lat = lat + kilometre
         long = long - kilometre * cos(radians(315))
 
-    if sens == 'sudsudest':
+    elif sens == 'sudsudest':
         lat = lat + kilometre
         long = long - kilometre * cos(radians(292.5))
 
@@ -54,10 +53,12 @@ def long_lat_1(sens, lat, long, kilometre):
 
 
 def long_lat(lat, long, kiloms, sens):
-    """We calculate new direction"""
-
-    #In case where superficie.py
-    #fail or an exception is present
+    """We calculate new direction for example if
+    direction is sud, we take the current latitude
+    we add the kilometer. This is our new latitude.
+    For north east, we subtracted kilometer to latitude
+    and longitude and for longitude
+    multiply by cos(337) radiant !"""
 
     try:
         kiloms = float(kiloms)
@@ -71,37 +72,35 @@ def long_lat(lat, long, kiloms, sens):
     if kiloms > 500:
         kiloms = 20.0
 
-    print(kiloms, sens)
-
     kilometre = kiloms * 0.009
 
     if sens == 'sud':
         lat1 = kilometre
         nouvel_lat = lat + lat1
-        return nouvel_lat, long
+        lat = nouvel_lat
 
-    if sens == 'nord':
+    elif sens == 'nord':
         lat1 = kilometre
         nouvel_lat = lat - lat1
-        return nouvel_lat, long
+        lat = nouvel_lat
 
-    if sens == 'ouest':
+    elif sens == 'ouest':
         long = long + kilometre
 
-    if sens == 'est':
+    elif sens == 'est':
         kilo = kilometre
         long1 = long - kilo
-        return lat, long1
+        long = long1
 
-    if sens == 'nordnordest':
+    elif sens == 'nordnordest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(337))
 
-    if sens == 'nordest':
+    elif sens == 'nordest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(315))
 
-    if sens == 'estnordest':
+    elif sens == 'estnordest':
         lat = lat - kilometre
         long = long - kilometre * cos(radians(292.5))
 
