499fa8862d880078d0377842dd5b5703bd00463a
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 18:04:52 +0200

    Update diagramme.py

--- bobo/polution/test/diagramme.py
+++ bobo/polution/test/diagramme.py
@@ -1,17 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
-
-
-
 def histogramme(x_data, y_data):
-    
-##    y = [i for i in y_data]
-##    x = [i for i in x_data]
 
-    y = [10,15,20,25,30,35,40,45,50,55]
+
+    y = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
     x = [2]
     
 
