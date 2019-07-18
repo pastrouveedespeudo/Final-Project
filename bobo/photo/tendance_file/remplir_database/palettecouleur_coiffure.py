92be4c62ca7f73ff6725e1b59496ba2e2b1b3f0b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 16:11:33 +0200

    Update palettecouleur_coiffure.py
    jpe pas vérifié j'ai plus de place dans la database.... fin si mais pour la prediction low low low cost... fin s'en est meme pas une ^^

--- bobo/photo/tendance_file/remplir_database/palettecouleur_coiffure.py
+++ bobo/photo/tendance_file/remplir_database/palettecouleur_coiffure.py
@@ -1,56 +1,32 @@
+"""Here we define pixel range
+for maroon, black and blond haircuts"""
 
-#faut refaire c chiant
 
 def couleur_cheuvelure(couleur1, couleur2, couleur3):
+    """We define color of haircut.
+    we playing with pixels.
+    """
+
+    out = ''
+    if couleur2 + 10 < couleur1 > couleur3 + 10 and\
+       50 < couleur1 < 130:
+        out = "marron"
+
+    #If couleur 1 superior to color2 and
+        #couleur1 > couleur3 and
+        #couleur1 > 50 and couleur 1 > 130
 
 
-    if couleur1 > couleur2 + 10 and\
-       couleur1 > couleur3 + 10 and\
-       couleur1 > 50 and\
-       couleur1 < 130:
-        return "marron"
-
-
-
-    if couleur1 < 50 and\
+    elif couleur1 < 50 and\
        couleur2 < 50 and\
        couleur3 < 50:
-        return "noir"
+        out = "noir"
 
-        
-    
-    if couleur1 > 90 and\
+    elif couleur1 > 90 and\
        couleur2 > 90 and\
        couleur3 > 90 and\
        couleur1 >= couleur2 + 10 and\
        couleur2 >= couleur3 + 10:
-        return "blond"
-        
+        out = "blond"
 
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return out
