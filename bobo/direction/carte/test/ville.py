713086d5b0a3dad432915ed5644b7f0fb82372bf
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 17:48:21 +0200

    Update ville.py

--- bobo/direction/carte/test/ville.py
+++ bobo/direction/carte/test/ville.py
@@ -1,61 +1,24 @@
-"""We call coordinate"""
+"""We give a parameter to the function.
+    it sends us a longitude and a lattitude"""
 
 from geopy.geocoders import Nominatim
-import requests
-from bs4 import *
-from math import *
-
 
 def ville(parametre):
-    """Here we searching from Python modul(geopy.geocoders)"""
-    """address from the input from html page"""
+    """We call geopy library. There is a timeout (30sec)
+    We geocode the parameter here a city,
+    call the address. We transform it into str variable
+    and return it."""
 
     geocoder = Nominatim(user_agent="app.py")
     #parametre is data recup from data()
-    
+
     location = geocoder.geocode(parametre, True, 30)
     localisation = location.address
     localisation = str(localisation)
 
     #define data from geopy.geocoders into var
 
-    b = location.latitude
-    c = location.longitude
+    lati = location.latitude
+    longi = location.longitude
 
-    
-    return b, c
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return lati, longi
