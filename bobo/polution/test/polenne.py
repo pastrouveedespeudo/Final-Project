8e4b69ca646bfda0cf275ad909f2d01647a31fac
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 18:07:22 +0200

    Update polenne.py

--- bobo/polution/test/polenne.py
+++ bobo/polution/test/polenne.py
@@ -1,55 +1,49 @@
-"""BS4, requests, datatime 16/04/2019"""
-
-
+"""We define polenne data"""
 
 
 import requests
+from bs4 import BeautifulSoup
 
-import urllib.request
-from bs4 import *
-
-
-
-from CONFIG_DATA_SITE import PATH_PARIS_POLENNE
-from CONFIG_DATA_SITE import PATH_LYON_POLENNE
-from CONFIG_DATA_SITE import PATH_MARSEILLE_POLENNE
-from CONFIG_DATA_SITE import COMPA_POELENNE
+from CONFIG import PATH_PARIS_POLENNE
+from CONFIG import PATH_LYON_POLENNE
+from CONFIG import PATH_MARSEILLE_POLENNE
+from CONFIG import COMPA_POELENNE
 
 
 def path_function(city):
     """Here we define the path"""
-    
+
     city = city.lower()
 
     path = ''
-    
+
     if city == 'lyon':
         path = PATH_LYON_POLENNE
 
     elif city == 'paris':
         path = PATH_PARIS_POLENNE
-        
+
     elif city == 'marseille':
         path = PATH_MARSEILLE_POLENNE
 
-    return path 
+    return path
 
 
 def soup_function(city):
     """Soup and BS4 call"""
-    
+
     #path_function()
     path = path_function(city)
 
     request_html = requests.get(path)
     page = request_html.content
     soup_html = BeautifulSoup(page, "html.parser")
-    Property = soup_html.find_all("div")
+    prperties = soup_html.find_all("div")
 
 
     liste = []
-    liste.append(str(Property))
-    
+    liste.append(str(prperties))
+
     finding = str(liste).find('Confort de respiration:')
     liste = liste[0][finding:finding + 200]
 
@@ -63,49 +57,21 @@ def polenne(city):
 
     word = ''
     liste2 = []
-    oContinuer = ''
-    
+    ocontinuer = ''
+
     for i in liste:
         if i == '>':
             if word == COMPA_POELENNE:
                 word = ''
-                oContinuer = True
+                ocontinuer = True
             else:
-                word =  ''
+                word = ''
 
-        elif oContinuer == True:
+        elif ocontinuer is True:
             if i == '<':
-                oContinuer = False
+                ocontinuer = False
             liste2.append(i)
         else:
             word += i
 
     return "".join(liste2[:-1])
-
-
-print(polenne('lyon'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
