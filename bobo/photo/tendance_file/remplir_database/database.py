7f19c478e4d2f98152980ef55aefb40729302350
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 16:06:03 +0200

    Update database.py

--- bobo/photo/tendance_file/remplir_database/database.py
+++ bobo/photo/tendance_file/remplir_database/database.py
@@ -1,23 +1,31 @@
+"""Here we create, delete,
+insert and visualisation tables"""
+
 import psycopg2
 
+from .CONFIG import DATABASE
+from .CONFIG import USER
+from .CONFIG import HOST
+from .CONFIG import PASSWORD
 
 
 
-class table:
+class Table:
+    """We create class table"""
 
-    def creation_database(self):
-        pass
-    #faudra la faire create database bobo
-    
-    def creation_table_donnée(self):
 
-        conn = psycopg2.connect(database='datu8fkornnndh',
-                                user='pwtfmpvfpsujtw',
-                                host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                                password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
+    @classmethod
+    def creation_table_donnee(cls):
+        """Create data table for top and bot of picture.
+        Stock name, size and color of the picture."""
+
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD)
 
         cur = conn.cursor()
-        
+
         cur.execute("""create table bobo1(
                     id serial PRIMARY KEY,
                     image varchar(100),
@@ -28,187 +36,193 @@ class table:
                     taille_haut int,
                     taille_bas int);
                     """)
-        
+
         conn.commit()
 
 
-    
-    def creation_table_donnée2(self):
 
-        conn = psycopg2.connect(database='datu8fkornnndh',
-                                user='pwtfmpvfpsujtw',
-                                host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                                password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
+    @classmethod
+    def creation_table_donnee2(cls):
+        """Create data table for top and bot of picture.
+        Stock name, size and color of the haircut picture."""
+
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD)
 
         cur = conn.cursor()
-        
+
         cur.execute("""create table bobo1_coiffure(
                     id serial PRIMARY KEY,
                     image varchar(100),
-                    coiffure varchar(100));
-                    """)
-        
+                    coiffure varchar(100));""")
+
         conn.commit()
 
 
-    def table_analyse(self):
+    @classmethod
+    def table_analyse(cls):
+        """We stocking the analysis of the picture"""
 
-        conn = psycopg2.connect(database='datu8fkornnndh',
-                                user='pwtfmpvfpsujtw',
-                                host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                                password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
-
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD)
         cur = conn.cursor()
-        
+
         cur.execute("""create table analyse_donnee1(
-                    analyse_donnee text);
-                    """)
-        
+                    analyse_donnee text);""")
+
         conn.commit()
 
 
-class insertion_table:
-     
-    def insertion_info(self, nom, sexe, haut, bas, taille_haut, taille_bas):
 
-        conn = psycopg2.connect(database='datu8fkornnndh',
-                                user='pwtfmpvfpsujtw',
-                                host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                                password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')
 
-        cur = conn.cursor()
+class Insertiontable:
+    """We create insertion class"""
 
-        
+
+    def __init__(self, taille_haut, taille_bas,
+                 nom, sexe, haut, bas,
+                 image):
+        """Initialise constructor"""
+
         self.taille_haut = taille_haut
         self.taille_bas = taille_bas
         self.nom = nom
         self.sexe = sexe
         self.haut = haut
         self.bas = bas
-        
-        
-        cur.execute("""insert into bobo1
-                 (image, sexe, haut, bas, taille_haut, taille_bas)
-                 values(%s, %s, %s, %s, %s, %s);""",
-                 (self.nom, self.sexe, self.haut, self.bas, self.taille_haut,
-                 taille_bas))
-
-        
-        
-        conn.commit()
-
-
-
-
-    def coiffure(self, image, coiffure):
-        
-        conn = psycopg2.connect(database='datu8fkornnndh',
-                                user='pwtfmpvfpsujtw',
-                                host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                                password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
-
-        cur = conn.cursor()
-        
-        self.coiffure = coiffure
         self.image = image
 
 
-        
+
+    def insertion_info(self, nom, sexe, haut,
+                       bas, taille_haut, taille_bas):
+        """Here we insert into database name, sexe, top, bot
+        and size of pictures"""
+
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD)
+
+        cur = conn.cursor()
+
+
+        self.taille_haut = taille_haut
+        self.taille_bas = taille_bas
+        self.nom = nom
+        self.sexe = sexe
+        self.haut = haut
+        self.bas = bas
+
+
+        cur.execute("""insert into bobo1
+                    (image, sexe, haut, bas, taille_haut, taille_bas)
+                     values(%s, %s, %s, %s, %s, %s);""",
+                    (self.nom, self.sexe,
+                     self.haut, self.bas,
+                     self.taille_haut,
+                     taille_bas))
+
+        conn.commit()
+
+
+
+    @classmethod
+    def coiffure(cls, image, coiffure):
+        """We insert data of haircuts pictures"""
+
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD)
+
+        cur = conn.cursor()
+
+        cls.coiffure = coiffure
+        cls.image = image
+
+
         cur.execute("""insert into bobo1_coiffure
                     (image, coiffure)
-                    values(%s, %s);""", (self.image, self.coiffure))
+                    values(%s, %s);""", (cls.image, cls.coiffure))
 
-        
 
         conn.commit()
 
 
 
 
-class visualisation_table:
-    def visualisation_donnée(self):
-        
-        conn = psycopg2.connect(database='datu8fkornnndh',
-                                user='pwtfmpvfpsujtw',
-                                host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                                password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
+class VisualisationTable:
+    """This is a visualisation class"""
+
+
+    @classmethod
+    def visualisation_donnee(cls):
+        """We visualise data (we add it to a list)"""
+
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD)
 
         cur = conn.cursor()
-        
+
         cur.execute("""SELECT * from bobo1""")
-                           
-        
+
+
         rows = cur.fetchall()
         liste = [i for i in rows]
 
 
         return liste
-        for i in liste:
-            print(i)
-            print('\n')
 
-    def visualisation_donnée2(self):
-        
-        conn = psycopg2.connect(database='datu8fkornnndh',
-                                user='pwtfmpvfpsujtw',
-                                host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                                password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
+
+
+    @classmethod
+    def visualisation_donnee2(cls):
+        """We visualise haircuts data (we add it to a list)"""
+
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD)
 
         cur = conn.cursor()
-        
+
         cur.execute("""SELECT * from bobo1_coiffure""")
-                           
-        
+
         rows = cur.fetchall()
         liste = [i for i in rows]
 
-
         return liste
-        for i in liste:
-            print(i)
-            print('\n')
-
 
 
 
 def supprimer_database():
-    
-    conn = psycopg2.connect(database='datu8fkornnndh',
-                            user='pwtfmpvfpsujtw',
-                            host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                            password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
+    """Here this is a delete function"""
+
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD)
 
     cur = conn.cursor()
-    
+
     cur.execute("""drop database bobo""")
-                       
-    
+
     conn.commit()
 
 
 
-
-
-
-#table = table()
-#table.table_analyse()
-#table.creation_table_donnée()
-#table.creation_table_donnée2()
-#visualisation_table = visualisation_table()
-#visualisation_table.visualisation_donnée()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+##if __name__ == "__main__":
+##
+##    table = Table()
+##    table.table_analyse()
+##    table.creation_table_donnee()
+##    table.creation_table_donnee2()
+##    visualisation_table = VisualisationTable()
+##    visualisation_table.Visualisation_onnee()
