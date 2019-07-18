f6bc600251e0fac8cce6d8acbbdbd4a12153f93b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-18 22:44:35 +0200

    Update test.py

--- bobo/photo/magasins/test/test.py
+++ bobo/photo/magasins/test/test.py
@@ -6,11 +6,7 @@ import views_functions as script4
 
 def test_ville():
     parametre = 'crest'
-    sortie = ['LOFT COIFFURE crest', 'Coiffure Marilyne',
-              'Tandem coiffure', 'coiffeur crest intermarché',
-              'coiffeur crest sans rdv', 'tandem coiffure crest',
-              'coiffeur barbier crest', 'coiffeur homme crest',
-              'marlene mon salon et moi', 'coiffeur valence']
+    sortie = ['LOFT COIFFURE crest', 'Coiffure Marilyne', 'Tandem coiffure', 'coiffeur crest intermarché', 'tandem coiffure crest', 'coiffeur barbier crest', 'coiffeur homme crest', 'marlene mon salon et moi', 'coiffeur montoison']
 
     assert script1.cities(parametre) == sortie
 
@@ -18,9 +14,10 @@ def test_ville():
 def test_ville2():
     parametre = 'LOFT COIFFURE crest'
     parametre1 = 'crest'
-    sortie = [['', 'Fermé'], ['', '09:30–18:30'], ['', '09:30–18:30'],
-              ['', '09:30–18:30'],['', '09:30–18:30'],
-              ['', '09:00–17:30'], ['', 'Fermé']]
+    sortie = [['', '09:30–18:30'], ['', '09:30–18:30'],
+              ['', '09:00–17:30'], ['', 'Fermé'],
+              ['', 'Fermé'], ['', '09:30–18:30'],
+              ['', '09:30–18:30']]
 
     assert script1.schedule_hair(parametre, parametre1) == sortie
 
@@ -45,10 +42,14 @@ def test_ville4():
 
 def test_ville5():
     parametre = 'crest'
-    sortie = ["Body's Studio", 'offre body studio', 'body studio le teil',
-              'salle de sport montelimar', 'salle de sport montelimar',
+    sortie = ["Body's Studio", 'aquagym crest',
+              'offre body studio', 'body studio le teil',
               'salle de sport montelimar',
-              'salle musculation le pouzin', 'salle musculation le pouzin']
+              'salle de sport montelimar',
+              'salle de sport montelimar',
+              'facebook body studio crest',
+              'salle musculation le pouzin',
+              'salle musculation le pouzin']
 
     assert script3.big_city_gym(parametre) == sortie
 
@@ -57,12 +58,13 @@ def test_ville5():
 def test_ville6():
     parametre = "Body's Studio"
     parametre1 = "crest"
-    sortie = [['lundi', '06:00–23:00'], ['mardi', '06:00–23:00'],
-              ['mercredi', '06:00–23:00'], ['jeudi', '06:00–23:00'],
-              ['vendredi', '06:00–23:00'],
-              ['samedi', '06:00–23:00'], ['dimanche', '06:00–23:00']]
+    sortie = [['jeudi', '06:00–23:00'], ['vendredi', '06:00–23:00'],
+              ['samedi', '06:00–23:00'], ['dimanche', '06:00–23:00'],
+              ['lundi', '06:00–23:00'],
+              ['mardi', '06:00–23:00'], ['mercredi', '06:00–23:00']]
 
     assert script3.schedule_gym(parametre, parametre1) == sortie
+
 
 
 def test_ville7():
@@ -120,25 +122,14 @@ def test_ville12():
 
 def test_ville12():
     parametre = 'Crest'
-    sortie = [["Body's Studio", [['lundi', '06:00–23:00'],
-              ['mardi', '06:00–23:00'],
-              ['mercredi', '06:00–23:00'], ['jeudi', '06:00–23:00'],
-              ['vendredi', '06:00–23:00'], ['samedi', '06:00–23:00'],
-              ['dimanche', '06:00–23:00']], ''], ['body studio le teil',
-              [['lundi', '06:00–23:00'], ['mardi', '06:00–23:00'], ['mercredi', '06:00–23:00'],
-              ['jeudi', '06:00–23:00'], ['vendredi', '06:00–23:00'], ['samedi', '06:00–23:00'],
-              ['dimanche', '06:00–23:00']], ''], ['salle de sport montelimar', [['lundi', '06:00–23:00'],
-              ['mardi', '06:00–23:00'], ['mercredi', '06:00–23:00'], ['jeudi', '06:00–23:00'],
-              ['vendredi', '06:00–23:00'], ['samedi', '06:00–23:00'], ['dimanche', '06:00–23:00']], ''],
-              ['salle de sport montelimar', [['lundi', '06:00–23:00'],
-              ['mardi', '06:00–23:00'], ['mercredi', '06:00–23:00'], ['jeudi', '06:00–23:00'], ['vendredi', '06:00–23:00'],
-              ['samedi', '06:00–23:00'], ['dimanche', '06:00–23:00']], '']]
-
+    sortie = [["Body's Studio", [['jeudi', '06:00–23:00'], ['vendredi', '06:00–23:00'], ['samedi', '06:00–23:00'], ['dimanche', '06:00–23:00'], ['lundi', '06:00–23:00'], ['mardi', '06:00–23:00'], ['mercredi', '06:00–23:00']], ''], ['body studio le teil', [['jeudi', '06:00–23:00'], ['vendredi', '06:00–23:00'], ['samedi', '06:00–23:00'], ['dimanche', '06:00–23:00'], ['lundi', '06:00–23:00'], ['mardi', '06:00–23:00'], ['mercredi', '06:00–23:00']], ''], ['salle de sport montelimar', [['jeudi', '06:00 – 23:00'], ['vendredi', '06:00 – 23:00'], ['samedi', '06:00 – 23:00'], ['dimanche', '06:00 – 23:00'], ['lundi', '06:00 – 23:00'], ['mardi', '06:00 – 23:00'], ['mercredi', '06:00 – 23:00']], '']]
     assert script4.gymm_function(parametre) == sortie
+
+
 
 def test_ville13():
     parametre = 'Crest'
-    sortie = [['LOFT COIFFURE crest', [['', 'Fermé'], ['', '09:30–18:30'], ['', '09:30–18:30'], ['', '09:30–18:30'], ['', '09:30–18:30'], ['', '09:00–17:30'], ['', 'Fermé']], ''], ['Tandem coiffure', [['', '09:00–18:00'], ['', '09:00–18:00'], ['', 'Fermé'], ['', '09:00–18:00'], ['', '09:00–18:00'], ['', '09:00–15:00'], ['', 'Fermé']], '']]
+    sortie = [['LOFT COIFFURE crest', [['', '09:30–18:30'], ['', '09:30–18:30'], ['', '09:00–17:30'], ['', 'Fermé'], ['', 'Fermé'], ['', '09:30–18:30'], ['', '09:30–18:30']], ''], ['Tandem coiffure', [['', '09:00–18:00'], ['', '09:00–18:00'], ['', '09:00–15:00'], ['', 'Fermé'], ['', '09:00–18:00'], ['', '09:00–18:00'], ['', 'Fermé']], ''], ['tandem coiffure crest', [['', '09:00–18:00'], ['', '09:00–18:00'], ['', '09:00–15:00'], ['', 'Fermé'], ['', '09:00–18:00'], ['', '09:00–18:00'], ['', 'Fermé']], '']]
     assert script4.haircut_style_function(parametre) == sortie
 
 
@@ -150,33 +141,3 @@ def test_ville14():
     sortie = '44.7350268 5.0103049'
     assert script4.map_hairdresser_function(parametre, parametre1) == sortie
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
