318cbcc2002877725396b9be68a2ca36ffd2aabe
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-19 22:38:11 +0200

    Update urls.py
    oups

--- bobo/bobo/urls.py
+++ bobo/bobo/urls.py
@@ -1,8 +1,15 @@
-
 from django.contrib import admin
 from django.urls import path
-from . import views
+
 from django.conf.urls import include
+from django.conf.urls import handler404
+from django.conf.urls import handler500
+
+from . import views
+
+
+handler404 = "bobo.views.handler404"
+handler500 = "bobo.views.handler500"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,10 +17,11 @@ urlpatterns = [
     path(r'/', views.home),
     path(r'photo', include('photo.urls')),
     path(r'polution', include('polution.urls')),
-    path(r'accounts', include('accounts.urls')),
-    path(r'administrateur', include('administrateur.urls')),
-    path(r'admin_pollu', include('admin_pollu.urls')),
     path(r'direction', include('direction.urls')),
     path(r'menu', views.menu),
-    
+    path(r'/error', views.handler404),
+    path(r'/error', views.handler500),
+    path(r'menu', views.menu),
+    path(r'mentions', views.mentions),
+    path(r'administrateur/mentions', views.mentions),
 ]
